bdd12cc7b8c8ec8092183c981027e1abe0be357f
Sofia Margariti <sofia.margariti@contentful.com>
2023-05-24 17:24:54 +0100

    feat: add empty state (#18)

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
   build: {
@@ -22,5 +23,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react(), dts()],
+  plugins: [svgr(), react(), dts()],
 })
