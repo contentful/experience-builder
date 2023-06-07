f783cdac139709dafe8b16c85570c30081bec826
Dimitry Kireyenkoff <dimitry.kireyenkoff@contentful.com>
2023-06-07 18:13:32 +0100

    refactor hook useExperiencBuilder:  - encapsulate all message validation logic within `doesMismatchMessageSchema()` - add clear error messages and reasons for message rejections  - avoid swallowing of rejected messages  - avoid swallowing of unsupported eventData.eventType  - avoid swallowing of usupported eventData.source  - validation and attaching strong type to the parsed message are done by tryParseMessage() after all the runtime checks   - in an attempt to make log messages filtrable adds prefix

---
+++ src/composabilityApp.ts
@@ -0,0 +1,75 @@
+import { IncomingExperienceBuilderEvent } from './types'
+
+export type VisualEditorMessagePayload = {
+  source: string
+  eventType: IncomingExperienceBuilderEvent
+  payload: any
+}
+
+class ParseError extends Error {
+  constructor(message: string) {
+    super(message)
+  }
+}
+
+const isValidJsonObject = (s: string) => {
+  try {
+    const result = JSON.parse(s)
+    if ('object' !== typeof result) {
+      return false
+    }
+    return true
+  } catch (e) {
+    return false
+  }
+}
+
+export const doesMismatchMessageSchema = (event: MessageEvent): false | string => {
+  try {
+    tryParseMessage(event)
+    return false
+  } catch (e) {
+    if (e instanceof ParseError) {
+      return e.message
+    }
+    throw e
+  }
+}
+
+export const tryParseMessage = (event: MessageEvent): VisualEditorMessagePayload => {
+  if (!event.data) {
+    throw new ParseError('Field event.data is missing')
+  }
+  if ('string' !== typeof event.data) {
+    throw new ParseError(`Field event.data must be string, instead '${typeof event.data}'`)
+  }
+
+  if (!isValidJsonObject(event.data)) {
+    throw new ParseError('Field event.data must be a valid JSON object serialized as string')
+  }
+
+  const eventData = JSON.parse(event.data)
+
+  if (!eventData.source) {
+    throw new ParseError(`Field eventData.source must be set and equal 'composability-app'`)
+  }
+
+  if ('composability-app' !== eventData.source) {
+    throw new ParseError(
+      `Field eventData.source must be equal 'composability-app', actual '${eventData.source}'`
+    )
+  }
+
+  // check eventData.eventType
+  const supportedEventTypes = Object.values(IncomingExperienceBuilderEvent)
+  console.log(`supportedEventTypes:`, supportedEventTypes)
+  if (!supportedEventTypes.includes(eventData.eventType)) {
+    throw new ParseError(
+      `Field eventData.eventType must be one of the supported values: [${supportedEventTypes.join(
+        ', '
+      )}]`
+    )
+  }
+
+  return eventData
+}
--- src/hooks/useExperienceBuilder.ts
+++ src/hooks/useExperienceBuilder.ts
@@ -9,36 +9,7 @@ import {
 } from '../types'
 import { useCommunication } from './useCommunication'
 import { getDataSourceFromTree } from '../utils'
-
-type VisualEditorMessagePayload = {
-  source: string
-  eventType: IncomingExperienceBuilderEvent
-  payload: any
-}
-
-const doesMismatchComposabilityAppMessageSchema = (event: MessageEvent): false | string => {
-  const isValidJson = (s: string) => {
-    try {
-      JSON.parse(s)
-      return true
-    } catch (e) {
-      return false
-    }
-  }
-
-  if (!event.data) {
-    return 'Field event.data is missing'
-  }
-  if ('string' !== typeof event.data) {
-    return `Field event.data must be string, instead '${typeof event.data}'`
-  }
-
-  if (!isValidJson(event.data)) {
-    return 'Field event.data must be valid JSON serialized representation of data'
-  }
-
-  return false
-}
+import { doesMismatchMessageSchema, tryParseMessage } from '../composabilityApp'
 
 export const useExperienceBuilder = () => {
   const [tree, setTree] = useState<CompositionTree>()
@@ -52,63 +23,57 @@ export const useExperienceBuilder = () => {
   useEffect(() => {
     const onMessage = (event: MessageEvent) => {
       let reason
-      if ((reason = doesMismatchComposabilityAppMessageSchema(event))) {
+      if ((reason = doesMismatchMessageSchema(event))) {
         console.warn(
-          `[eb.sdk] Ignoring alien incoming message from origin [${event.origin}], due to: [${reason}]`,
+          `[eb.sdk::onMessage] Ignoring alien incoming message from origin [${event.origin}], due to: [${reason}]`,
           event
         )
         return
       }
 
-      // @ts-expect-error not typed
-      let eventData: VisualEditorMessagePayload = {}
-      try {
-        if (event.data && typeof event.data === 'string') {
-          eventData = JSON.parse(event.data)
-        }
-      } catch (e) {
-        console.log('event data caused error', event.data)
-      }
-      console.log('customer app received message', eventData)
+      let eventData = tryParseMessage(event)
 
-      if (eventData.source === 'composability-app') {
-        const { payload } = eventData
+      console.log(`[eb.sdk::onMessage] Received message [${eventData.eventType}]`, eventData)
 
-        switch (eventData.eventType) {
-          case IncomingExperienceBuilderEvent.COMPOSITION_UPDATED: {
-            const { tree, locale } = payload
-            setTree(tree)
-            setLocale(locale)
-            setDataSource(getDataSourceFromTree(tree))
-            break
-          }
-          case IncomingExperienceBuilderEvent.SELECTED_COMPONENT_CHANGED: {
-            const { selectedNodeId } = payload
-            setSelectedNodeId(selectedNodeId)
-            break
-          }
-          case IncomingExperienceBuilderEvent.COMPONENT_VALUE_CHANGED: {
-            /** TODO: at the moment, not sure how to best handle this case.
-             * we need to know the variable name, component id, locale
-             * should experience builder update the tree and send the updated tree?
-             * should we update it here instead of going over the whole tree again?
-             * getDataSourceFromTree(tree)
-             *
-             * Currently experience builder (user_interface) puts default value into dataSource
-             * and marks it with `type: UnboundValue`
-             *
-             * If there has been no defaultValue, then there will be no entry with uuid in dataSource
-             *
-             */
-            break
-          }
-          case IncomingExperienceBuilderEvent.COMPONENT_DRAGGING_CHANGED: {
-            const { isDragging } = payload
-            setIsDragging(isDragging)
-            break
-          }
-          default:
+      const { payload } = eventData
+
+      switch (eventData.eventType) {
+        case IncomingExperienceBuilderEvent.COMPOSITION_UPDATED: {
+          const { tree, locale } = payload
+          setTree(tree)
+          setLocale(locale)
+          setDataSource(getDataSourceFromTree(tree))
+          break
         }
+        case IncomingExperienceBuilderEvent.SELECTED_COMPONENT_CHANGED: {
+          const { selectedNodeId } = payload
+          setSelectedNodeId(selectedNodeId)
+          break
+        }
+        case IncomingExperienceBuilderEvent.COMPONENT_VALUE_CHANGED: {
+          /** TODO: at the moment, not sure how to best handle this case.
+           * we need to know the variable name, component id, locale
+           * should experience builder update the tree and send the updated tree?
+           * should we update it here instead of going over the whole tree again?
+           * getDataSourceFromTree(tree)
+           *
+           * Currently experience builder (user_interface) puts default value into dataSource
+           * and marks it with `type: UnboundValue`
+           *
+           * If there has been no defaultValue, then there will be no entry with uuid in dataSource
+           *
+           */
+          break
+        }
+        case IncomingExperienceBuilderEvent.COMPONENT_DRAGGING_CHANGED: {
+          const { isDragging } = payload
+          setIsDragging(isDragging)
+          break
+        }
+        default:
+          console.error(
+            `[eb.sdk::onMessage] Logic error, unsupported eventType: [${eventData.eventType}]`
+          )
       }
     }
 
