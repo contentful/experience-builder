9fc41bd0d17c369f1a003024be4fe623a4479baa
Sofia Margariti <sofia.margariti@contentful.com>
2023-06-29 08:23:46 +0100

    Remove outdated references to emotion/forma36

--- src/blocks/EmptyDeliveryContainer.tsx
+++ src/blocks/EmptyDeliveryContainer.tsx
@@ -1,8 +1,4 @@
-import { css, cx } from '@emotion/css'
-import { ReactComponent as EmptyStateIcon } from './emptyState.svg'
 import React from 'react'
-import tokens from '@contentful/f36-tokens'
-import { useInteraction } from '../hooks'
 
 export const EmptyDeliveryContainer = () => {
   // TODO replace with a meaningful component or message
