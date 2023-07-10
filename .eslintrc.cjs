a50ac43dcf9bfef826b078c118847cf94992549e
Chidinma orajiaku <corajiaku96@gmail.com>
2023-07-10 17:02:36 +0200

    fix: line format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,28 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-				"plugin:react-hooks/recommended",
-        "plugin:@typescript-eslint/recommended",
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-				"react-hooks",
-        "@typescript-eslint"
-    ],
-    "rules": {
-      'react-hooks/exhaustive-deps': 'warn',
-    }
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  rules: {
+    'react-hooks/exhaustive-deps': 'warn',
+  },
 }
