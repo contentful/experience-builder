b06010fd5bbdffd1452c7650a21398a907065a10
Lorenz <lorenz.weis@gmail.com>
2023-07-31 17:10:16 +0200

    refactor: add scroll state to apge

--- src/communication/HoverIndicatorHandler.ts
+++ src/communication/HoverIndicatorHandler.ts
@@ -9,19 +9,20 @@ import { sendMessage } from './sendMessage'
 export class HoverIndicatorHandler {
   private getCoordinatesOfElement(element: HTMLElement | Element): RawCoordinates {
     const { left, top, width, height } = element.getBoundingClientRect()
-    const { pageXOffset, pageYOffset } = window
+    const { pageXOffset, pageYOffset, scrollX, scrollY } = window
+    console.log(window.scrollX)
+    console.log(window.scrollY)
 
     return {
-      left: left + pageXOffset,
-      top: top + pageYOffset,
+      left: left + pageXOffset - scrollX,
+      top: top + pageYOffset - scrollY,
       width,
       height,
     }
   }
 
-  private getFullCoordinates = (element: HTMLElement, event: MouseEvent) => {
+  private getFullCoordinates = (element: HTMLElement) => {
     const rawCoordinates = this.getCoordinatesOfElement(element)
-    const { left, top } = element.getBoundingClientRect()
 
     const childrenCoordinates: RawCoordinates[] = Array.from(element.children)
       .filter((child) => child.id !== 'SectionTooltip')
@@ -33,12 +34,12 @@ export class HoverIndicatorHandler {
     }
   }
 
-  private getHoveredElement = (event: MouseEvent) => {
+  private getHoveredElement = (element: HTMLElement | null) => {
     let coordinates: Coordinates | null = null
     let parentElement: HoveredElement | null = null
     let hoveredElement: HoveredElement | null = null
     let parentSectionIndex = -1
-    let target = event.target as HTMLElement | null
+    let target = element
 
     // If the target is outside on the root or anywhere else on the iframes body
     if (target?.id === 'VisualEditorRoot' || target?.tagName === 'BODY') {
@@ -49,7 +50,7 @@ export class HoverIndicatorHandler {
         blockId: 'root',
       }
       if (rootElement) {
-        coordinates = this.getFullCoordinates(rootElement, event)
+        coordinates = this.getFullCoordinates(rootElement)
       }
     } else {
       // Find the closest contentful container or direct parent that is a contentful container
@@ -60,7 +61,7 @@ export class HoverIndicatorHandler {
           // Or a direct child of a section
           (target.parentElement && target.parentElement.dataset.cfNodeBlockType === 'block')
         ) {
-          coordinates = this.getFullCoordinates(target, event)
+          coordinates = this.getFullCoordinates(target)
 
           const sectionId = target.dataset.cfNodeId
           const sectionBlockId = target.dataset.cfNodeBlockId
@@ -101,15 +102,18 @@ export class HoverIndicatorHandler {
     return { coordinates, hoveredElement, parentElement, parentSectionIndex }
   }
 
-  handleMouseMove = (event: MouseEvent): void => {
+  handleMouseMove = (target: HTMLElement | null, x: number, y: number): void => {
     const mousePosInTarget: { x: number; y: number } = { x: 0, y: 0 }
 
     const { coordinates, hoveredElement, parentElement, parentSectionIndex } =
-      this.getHoveredElement(event)
+      this.getHoveredElement(target)
+
+    console.log(window.scrollX)
+    console.log(window.scrollY)
 
     if (coordinates) {
-      mousePosInTarget.x = event.clientX - coordinates.left
-      mousePosInTarget.y = event.clientY - coordinates.top
+      mousePosInTarget.x = x - coordinates.left
+      mousePosInTarget.y = y - coordinates.top
     }
 
     sendMessage(OutgoingExperienceBuilderEvent.MOUSE_MOVE, {
@@ -118,16 +122,22 @@ export class HoverIndicatorHandler {
       parentSectionIndex,
       mousePosInTarget,
       coordinates,
-      clientX: event.clientX,
-      clientY: event.clientY,
+      clientX: x,
+      clientY: y,
     })
   }
 
+  onMouseMove = (event: MouseEvent) => {
+    const target: HTMLElement | null = event.target as HTMLElement
+    const [x, y] = [event.clientX, event.clientY]
+    this.handleMouseMove(target, x, y)
+  }
+
   attachEvent(): void {
-    document.addEventListener('mousemove', this.handleMouseMove)
+    document.addEventListener('mousemove', this.onMouseMove)
   }
 
   detachEvent(): void {
-    document.removeEventListener('mousemove', this.handleMouseMove)
+    document.removeEventListener('mousemove', this.onMouseMove)
   }
 }
