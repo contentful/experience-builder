debaf08462d759ee48e738ee08e06a43ef952c3d
Daniel Vasylenko <connect.danv@gmail.com>
2023-08-10 11:44:19 +0200

    feat: script to publish the sdk to the github package registry

---
+++ bin/release-ctfl-eap.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+git fetch -p
+
+# get the lates tag that will be the version which will be used
+latestTag=$(git describe --tags) # is in format of v{version}
+latestVersion=${latestTag:1} # drops the v and stores the {version}
+
+# writing this line into a newly ceated local .npmrc
+echo "@ctfl-eap:registry=https://npm.pkg.github.com/ctfl-eap" > .npmrc
+ 
+# replacing package.version with latest tag and changing package name
+jq --arg latestVersion "$latestVersion" '.name="@ctfl-eap/experience-builder" | .version=$latestVersion' package.json > temp.json && mv temp.json package.json
+
+yarn build
+
+## appending the new registry info to the global .npmrc
+echo "//npm.pkg.github.com/:_authToken=$CTFL_EAP_GITBUB_WRITE_TOKEN" >> ~/.npmrc
+
+cat ~/.npmrc
+# npm publish
