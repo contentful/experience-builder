273a3eaf8e19f63f77ae3a5fd1ca4588e78b2c72
Daniel Vasylenko <connect.danv@gmail.com>
2023-08-10 13:47:28 +0200

    fix: use cut to get substring

--- bin/release-ctfl-eap.sh
+++ bin/release-ctfl-eap.sh
@@ -3,11 +3,11 @@ git fetch -p
 
 # get the lates tag that will be the version which will be used
 latestTag=$(git describe --tags) # is in format of v{version}
-latestVersion="${latestTag:1}" # drops the v and stores the {version}
+latestVersion=$(cut -c 2- <<< $latestTag) # drops the v and stores the {version}
 
 # writing this line into a newly ceated local .npmrc
 echo "@ctfl-eap:registry=https://npm.pkg.github.com/ctfl-eap" > .npmrc
- 
+
 # replacing package.version with latest tag and changing package name
 jq --arg latestVersion "$latestVersion" '.name="@ctfl-eap/experience-builder" | .version=$latestVersion' package.json > temp.json && mv temp.json package.json
 
