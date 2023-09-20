5d25706bf867e88f116fe573ad07d97da710365e
Ely Lucas <ely@meta-tek.net>
2023-09-20 15:06:42 -0600

    adding option to not wrap components with withExperienceBuilder [] (#9)
    * adding option to not wrap components with withExperienceBuilder []
    
    * renaming ebProps to cfProps

--- components/src/utils/withExperienceBuilder.cy.tsx
+++ components/src/utils/withExperienceBuilder.cy.tsx
@@ -3,33 +3,156 @@ import { Button } from '@components/Button';
 import { withExperienceBuilder } from './withExperienceBuilder';
 
 describe('withExperienceBuilder', () => {
-  const ExperienceBuilderButton = withExperienceBuilder(Button, {
-    id: 'button',
-    name: 'Button',
-    category: 'Components',
-    variables: {},
-  });
-
-  it('classes should be applied to the inner component', () => {
-    cy.mount(<ExperienceBuilderButton classes="test" label="Click me" />);
-    cy.get('button').should('have.class', 'test');
-  });
-
-  it('events should be bound to the container div', () => {
-    const onClickSpy = cy.spy().as('onClickSpy');
-    const onMouseDownSpy = cy.spy().as('onMouseDownSpy');
-    const onMouseUpSpy = cy.spy().as('onMouseUpSpy');
-    cy.mount(
-      <ExperienceBuilderButton
-        onClick={onClickSpy}
-        onMouseDown={onMouseDownSpy}
-        onMouseUp={onMouseUpSpy}
-        label="Click me"
-      />
+  describe('when component is wrapped', () => {
+    const ExperienceBuilderButton = withExperienceBuilder(
+      Button,
+      {
+        id: 'button',
+        name: 'Button',
+        category: 'Components',
+        variables: {},
+      },
+      { wrapComponent: true }
     );
-    cy.get('div').first().click();
-    cy.get('@onClickSpy').should('have.been.calledOnce');
-    cy.get('@onMouseDownSpy').should('have.been.calledOnce');
-    cy.get('@onMouseUpSpy').should('have.been.calledOnce');
+
+    it('classes should be applied to the inner component', () => {
+      cy.mount(<ExperienceBuilderButton classes="test" label="Click me" />);
+      cy.get('button').should('have.class', 'test');
+    });
+
+    it('events should be bound to the container div', () => {
+      const onClickSpy = cy.spy().as('onClickSpy');
+      const onMouseDownSpy = cy.spy().as('onMouseDownSpy');
+      const onMouseUpSpy = cy.spy().as('onMouseUpSpy');
+      cy.mount(
+        <ExperienceBuilderButton
+          onClick={onClickSpy}
+          onMouseDown={onMouseDownSpy}
+          onMouseUp={onMouseUpSpy}
+          label="Click me"
+          className="my-div" //so we can select it later
+        />
+      );
+      cy.get('div.my-div').click();
+      cy.get('@onClickSpy').should('have.been.calledOnce');
+      cy.get('@onMouseDownSpy').should('have.been.calledOnce');
+      cy.get('@onMouseUpSpy').should('have.been.calledOnce');
+    });
+
+    it('extra props should be passed to the container div', () => {
+      cy.mount(
+        <ExperienceBuilderButton
+          label="Click me"
+          data-cf-node-block-id="test1"
+          data-cf-node-block-type="test2"
+          data-cf-node-id="test3"
+          className="my-div" //so we can select it later
+        />
+      );
+
+      cy.get('div.my-div').should('have.attr', 'data-cf-node-block-id', 'test1');
+      cy.get('div.my-div').should('have.attr', 'data-cf-node-block-type', 'test2');
+      cy.get('div.my-div').should('have.attr', 'data-cf-node-id', 'test3');
+    });
+
+    it('can wrap a component with a custom tag', () => {
+      const ExperienceBuilderButtonSpan = withExperienceBuilder(
+        Button,
+        {
+          id: 'button',
+          name: 'Button',
+          category: 'Components',
+          variables: {},
+        },
+        { wrapComponent: true, wrapContainerTag: 'span' }
+      );
+      cy.mount(
+        <ExperienceBuilderButtonSpan
+          label="Click me"
+          className="my-span" //so we can select it later
+        />
+      );
+      cy.get('span.my-span').should('exist').find('button').contains('Click me');
+    });
+
+    it('classes get added to the correct elements', () => {
+      cy.mount(
+        <ExperienceBuilderButton
+          label="Click me"
+          data-cf-node-block-id="test1"
+          data-cf-node-block-type="test2"
+          data-cf-node-id="test3"
+          className="my-div" //so we can select it later
+          classes="my-class"
+        />
+      );
+      cy.get('div.my-div').should('have.class', 'my-div');
+      cy.get('button').should('have.class', 'my-class');
+    });
+  });
+
+  describe('when component is not wrapped', () => {
+    const ExperienceBuilderButton = withExperienceBuilder(
+      Button,
+      {
+        id: 'button',
+        name: 'Button',
+        category: 'Components',
+        variables: {},
+      },
+      { wrapComponent: false }
+    );
+
+    it('classes should be applied to the component itself', () => {
+      cy.mount(<ExperienceBuilderButton classes="test" label="Click me" />);
+      cy.get('button').should('have.class', 'test');
+    });
+
+    it('events should be bound to the component itself', () => {
+      const onClickSpy = cy.spy().as('onClickSpy');
+      const onMouseDownSpy = cy.spy().as('onMouseDownSpy');
+      const onMouseUpSpy = cy.spy().as('onMouseUpSpy');
+      cy.mount(
+        <ExperienceBuilderButton
+          onClick={onClickSpy}
+          onMouseDown={onMouseDownSpy}
+          onMouseUp={onMouseUpSpy}
+          label="Click me"
+        />
+      );
+      cy.get('button').first().click();
+      cy.get('@onClickSpy').should('have.been.calledOnce');
+      cy.get('@onMouseDownSpy').should('have.been.calledOnce');
+      cy.get('@onMouseUpSpy').should('have.been.calledOnce');
+    });
+
+    it('extra props should be passed to the component', () => {
+      cy.mount(
+        <ExperienceBuilderButton
+          label="Click me"
+          data-cf-node-block-id="test1"
+          data-cf-node-block-type="test2"
+          data-cf-node-id="test3"
+        />
+      );
+      cy.get('button').should('have.attr', 'data-cf-node-block-id', 'test1');
+      cy.get('button').should('have.attr', 'data-cf-node-block-type', 'test2');
+      cy.get('button').should('have.attr', 'data-cf-node-id', 'test3');
+    });
+
+    it('classes get added to the correct elements', () => {
+      cy.mount(
+        <ExperienceBuilderButton
+          label="Click me"
+          data-cf-node-block-id="test1"
+          data-cf-node-block-type="test2"
+          data-cf-node-id="test3"
+          className="my-button" //so we can select it later
+          classes="my-class"
+        />
+      );
+      cy.get('button').should('have.class', 'my-button');
+      cy.get('button').should('have.class', 'my-class');
+    });
   });
 });
--- components/src/utils/withExperienceBuilder.tsx
+++ components/src/utils/withExperienceBuilder.tsx
@@ -3,42 +3,96 @@ import { ComponentDefinition } from '@contentful/experience-builder';
 import React from 'react';
 
 export interface CFProps {
+  /**
+   * Classes to be applied to the container component if `wrapComponent` is true, or directly to the child component if false.
+   */
   className?: string;
+  /**
+   * Classes to be applied to the child component if `wrapComponent` is true, or directly to the child component if false.
+   */
   classes?: string;
-  onMouseDown?: React.MouseEventHandler<HTMLDivElement>;
-  onMouseUp?: React.MouseEventHandler<HTMLDivElement>;
-  onClick?: React.MouseEventHandler<HTMLDivElement>;
+  onMouseDown?: React.MouseEventHandler;
+  onMouseUp?: React.MouseEventHandler;
+  onClick?: React.MouseEventHandler;
+  /**
+   * Prop required by Experience Builder to identify the component.
+   */
   'data-cf-node-id'?: string;
+  /**
+   * Prop required by Experience Builder to identify the component.
+   */
   'data-cf-node-block-id'?: string;
+  /**
+   * Prop required by Experience Builder to identify the component's type.
+   */
   'data-cf-node-block-type'?: string;
 }
 
+export interface WithExperienceBuilderOptions {
+  /**
+   * If true, the component will be wrapped with a container component.If false, the component will be rendered directly with no container.
+   * @default false
+   * @optional
+   */
+  wrapComponent?: boolean;
+  /**
+   * If `wrapComponent` is true, this will be the tag of the container component.
+   * @default 'div'
+   * @optional
+   */
+  wrapContainerTag?: keyof JSX.IntrinsicElements;
+}
+
+/**
+ * Sets up a component to be consumed by Experience Builder. This function can be used to wrap a component with a container component, or to pass props to the component directly.
+ * @param Component Component to be used by Experience Builder.
+ * @param componentDefinition ComponentDefinition object to be used by Experience Builder.
+ * @param options Options for the `withExperienceBuilder` function.
+ * @default { wrapComponent: false, wrapContainerTag: 'div' }
+ * @returns A component that can be passed to `defineComponents` in Experience Builder.
+ */
 export function withExperienceBuilder<T extends object>(
   Component: React.ComponentType<T>,
-  options: ComponentDefinition
+  componentDefinition: ComponentDefinition,
+  options: WithExperienceBuilderOptions = {
+    wrapComponent: false,
+    wrapContainerTag: 'div',
+  }
 ) {
   const Wrapped: CompositionComponent<CFProps & T> = ({
-    classes,
-    className,
-    onMouseDown,
-    onMouseUp,
-    onClick,
+    classes = '',
+    className = '',
     'data-cf-node-id': dataCfNodeId,
     'data-cf-node-block-id': dataCfNodeBlockId,
     'data-cf-node-block-type': dataCfNodeBlockType,
+    onClick,
+    onMouseDown,
+    onMouseUp,
     ...props
-  }) => (
-    <div
-      className={className}
-      onMouseDown={onMouseDown}
-      onMouseUp={onMouseUp}
-      onClick={onClick}
-      data-cf-node-id={dataCfNodeId}
-      data-cf-node-block-id={dataCfNodeBlockId}
-      data-cf-node-block-type={dataCfNodeBlockType}>
-      <Component className={classes} {...(props as T)} />
-    </div>
-  );
-  Wrapped.ComponentDefinition = options;
+  }) => {
+    const Tag = options.wrapContainerTag || 'div';
+    const cfProps = {
+      'data-cf-node-id': dataCfNodeId,
+      'data-cf-node-block-id': dataCfNodeBlockId,
+      'data-cf-node-block-type': dataCfNodeBlockType,
+      onClick,
+      onMouseDown,
+      onMouseUp,
+    };
+    const component = options.wrapComponent ? (
+      <Tag className={className} {...cfProps}>
+        <Component className={classes} {...(props as T)} />
+      </Tag>
+    ) : (
+      React.createElement(Component, {
+        className: classes + className ? classes + ' ' + className : undefined,
+        ...cfProps,
+        ...(props as T),
+      })
+    );
+    return component;
+  };
+
+  Wrapped.ComponentDefinition = componentDefinition;
   return Wrapped;
 }
