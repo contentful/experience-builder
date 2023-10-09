de3f58d45f01c2cc6fd8b8a9007bcd5ca999f427
Sofia Margariti <sofia.margariti@contentful.com>
2023-10-09 16:53:18 +0100

    feat: send SDK errors to web app via postMessage

--- src/blocks/ErrorBoundary.tsx
+++ src/blocks/ErrorBoundary.tsx
@@ -1,5 +1,9 @@
 import React, { ErrorInfo, ReactElement } from 'react'
+import { sendMessage } from '../communication/sendMessage'
+import { OutgoingExperienceBuilderEvent } from '../types'
 import './ErrorBoundary.css'
+
+class ImportedComponentError extends Error {}
 
 export class ErrorBoundary extends React.Component<
   { children: ReactElement },
@@ -16,8 +20,9 @@ export class ErrorBoundary extends React.Component<
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
     this.setState({ error, errorInfo })
-    console.error(error, errorInfo)
-    // let's catch this error somehow?
+    if (!(error instanceof ImportedComponentError)) {
+      sendMessage(OutgoingExperienceBuilderEvent.CANVAS_ERROR, error)
+    }
   }
 
   render() {
@@ -29,6 +34,18 @@ export class ErrorBoundary extends React.Component<
         </div>
       )
     }
+    return this.props.children
+  }
+}
+
+export class ImportedComponentErrorBoundary extends React.Component<{ children: ReactElement }> {
+  componentDidCatch(error: Error, _errorInfo: ErrorInfo) {
+    const err = new ImportedComponentError(error.message)
+    err.stack = error.stack
+    throw err
+  }
+
+  render() {
     return this.props.children
   }
 }
