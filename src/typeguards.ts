dfeb077bbd88a4eee6b79b37cb72987afceb1606
Daniel Vasylenko <connect.danv@gmail.com>
2023-10-16 17:57:42 +0200

    test: basic PreviewDeliveryRoot test

--- src/typeguards.ts
+++ src/typeguards.ts
@@ -4,18 +4,18 @@ import type { DeprecatedExperience, Experience, ExperienceEntry } from './types'
 // @ts-expect-error type incompatibility
 export const isExperienceEntry = (entry: Entry): entry is ExperienceEntry => {
   return (
-    entry.sys.type === 'Entry' &&
-    !!entry.fields.title &&
-    !!entry.fields.slug &&
-    !!entry.fields.componentTree &&
+    entry?.sys?.type === 'Entry' &&
+    !!entry.fields?.title &&
+    !!entry.fields?.slug &&
+    !!entry.fields?.componentTree &&
     // @ts-expect-error type incompatibility due to conditional types in Entry
     Array.isArray(entry.fields.componentTree.breakpoints) &&
     // @ts-expect-error type incompatibility due to conditional types in Entry
     Array.isArray(entry.fields.componentTree.children) &&
     // @ts-expect-error type incompatibility due to conditional types in Entry
     typeof entry.fields.componentTree.schemaVersion === 'string' &&
-    !!entry.fields.dataSource &&
-    !!entry.fields.unboundValues
+    !!entry.fields?.dataSource &&
+    !!entry.fields?.unboundValues
   )
 }
 
