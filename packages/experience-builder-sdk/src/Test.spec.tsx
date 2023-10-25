6ab1306284958f15d8a9b2a68750be17924fed28
Anwar <anwaar.lance@gmail.com>
2023-10-25 16:45:13 +0200

    test: setting max workers

--- packages/experience-builder-sdk/src/Test.spec.tsx
+++ packages/experience-builder-sdk/src/Test.spec.tsx
@@ -1,3 +1,9 @@
+import React from 'react'
+
+import { render } from '@testing-library/react'
+
+import { Test } from './Test'
+
 jest.mock('./core/constants', () => ({
   VITE_SDK_VERSION: '0.0.0-test',
 }))
@@ -5,5 +11,11 @@ jest.mock('./core/constants', () => ({
 describe('test', () => {
   test('should be ok', () => {
     expect(true).toBe(true)
+  })
+
+  test('should also be ok', () => {
+    const { getByTestId } = render(<Test />)
+    expect(getByTestId('test')).toBeInTheDocument()
+    expect(getByTestId('test')).toHaveTextContent('Test')
   })
 })
