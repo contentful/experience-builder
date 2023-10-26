ea4880bcf27a005146b910e68abe2372d28a4eee
Chase Poirier <chase.poirier@contentful.com>
2023-10-26 13:55:33 -0600

    chore: rename storybook package

--- test-app/.storybook/main.ts
+++ test-app/.storybook/main.ts
@@ -16,7 +16,7 @@ const config: StorybookConfig = {
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@storybook/addon-onboarding'),
     getAbsolutePath('@storybook/addon-interactions'),
-    getAbsolutePath('@contentful/experience-builder-addon'),
+    getAbsolutePath('@contentful/experience-builder-storybook-addon'),
   ],
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),
