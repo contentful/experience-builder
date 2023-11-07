6644b29bf8b9c19572789d67ce7791bb7db061f3
Ely Lucas <ely@meta-tek.net>
2023-11-07 11:12:30 -0700

    fix(experience-builder-sdk): export ExternalSDKMode type

--- packages/experience-builder-sdk/src/index.ts
+++ packages/experience-builder-sdk/src/index.ts
@@ -6,6 +6,7 @@ export type {
   ComponentDefinition,
   ComponentRegistration,
   Experience,
+  ExternalSDKMode,
   DeprecatedExperience,
   ComponentDefinitionVariable,
   ComponentDefinitionVariableType,
