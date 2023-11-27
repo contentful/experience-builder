c4051169d115abad1bf2056a3fdf2ebbbbf67d2e
Ely Lucas <ely@meta-tek.net>
2023-11-27 16:11:49 -0700

    chore: add VisualEditorContext back in

--- packages/experience-builder-sdk/src/blocks/VisualEditor.tsx
+++ packages/experience-builder-sdk/src/blocks/VisualEditor.tsx
@@ -1,28 +1,23 @@
 import React from 'react';
-import { ComponentRegistration, InternalSDKMode } from '../types';
-import { VisualEditorContextProvider } from './editor/VisualEditorContext';
+import { InternalSDKMode } from '../types';
 import { componentRegistry } from '../core/componentRegistry';
-
-const VisualEditor = React.lazy(async () => {
-  const { VisualEditor } = await import('@contentful/experience-builder-visual-editor');
-  return { default: VisualEditor };
-});
+import VisualEditor from '@contentful/experience-builder-visual-editor';
+import { VisualEditorContextProvider } from './editor/VisualEditorContext';
 
 type VisualEditorRootProps = {
   initialLocale: string;
   mode: InternalSDKMode;
 };
 
-export const VisualEditorRoot = ({ initialLocale, mode }: VisualEditorRootProps) => {
-  const components: ComponentRegistration[] = [];
-
-  componentRegistry.forEach((comp) => {
-    components.push(comp);
-  });
+export const VisualEditorRoot: React.FC<VisualEditorRootProps> = ({ initialLocale, mode }) => {
   // in editor mode locale can change via sendMessage from web app, hence we use the locale from props only as initial locale
   return (
     <VisualEditorContextProvider mode={mode} initialLocale={initialLocale}>
-      <VisualEditor components={components} />
+      <VisualEditor
+        mode={'editor'}
+        initialLocale={initialLocale}
+        initialComponentRegistry={componentRegistry}
+      />
     </VisualEditorContextProvider>
   );
 };
