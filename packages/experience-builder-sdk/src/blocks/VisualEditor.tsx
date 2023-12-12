eeb4c6b263cf733a856d93beaa9f01e4f08523fd
Adrian Meyer <8539634+primeinteger@users.noreply.github.com>
2023-12-12 13:31:42 -0700

    wip: remove old commented out attempts

--- packages/experience-builder-sdk/src/blocks/VisualEditor.tsx
+++ packages/experience-builder-sdk/src/blocks/VisualEditor.tsx
@@ -3,40 +3,6 @@ import { InternalSDKMode } from '../types';
 import { VisualEditorContextProvider } from './editor/VisualEditorContext';
 import { ErrorBoundary } from '../components/ErrorBoundary';
 
-//============================================================
-// Import the visual editor directly from the package
-// import VisualEditor from '@contentful/experience-builder-visual-editor';
-
-//============================================================
-// Lazy load the visual editor from the package
-// const VisualEditor = React.lazy(() => import('@contentful/experience-builder-visual-editor'));
-
-//============================================================
-// Lazy load the visual editor from a CDN - this is the ideal solution if we can get it to work
-// const VisualEditor = React.lazy(
-//   () =>
-//     import(
-//       'https://unpkg.com/@contentful/experience-builder-visual-editor@0.0.1-pre-20231212T111240.0/dist/index.js'
-//     )
-// );
-
-//============================================================
-// Lazy load the visual editor from a CDN with a specific version
-// const VisualEditor = React.lazy(
-//   () =>
-//     import(
-//       'https://unpkg.com/@contentful/experience-builder-visual-editor@0.0.1-pre-20231208T034641.0/dist/index.js'
-//     )
-// );
-
-// type VisualEditorRootProps = {
-//   initialLocale: string;
-//   initialComponentRegistry: Map<string, ComponentRegistration>;
-// };
-
-//============================================================
-// Inject script
-
 // const scriptUrl = 'https://unpkg.com/@contentful/experience-builder-visual-editor';
 // const scriptUrl =
 //   'https://unpkg.com/@contentful/experience-builder-visual-editor@0.0.1-pre-20231208T034641.0/dist/index.js';
@@ -44,31 +10,14 @@ import { ErrorBoundary } from '../components/ErrorBoundary';
 const scriptUrl =
   '/Users/adrian.meyer/Repos/experience-builder-toolkit/packages/visual-editor/dist/renderApp.js';
 
-// const loaded = new Map<string, string>();
-
 const VisualEditor: React.FC<{ elementId: string }> = ({ elementId }) => {
   useEffect(() => {
     console.log('[SDK::DEBUG] VisualEditor useEffect...');
-
-    // if (loaded.get(initialLocale) === scriptUrl) {
-    //   return;
-    // }
-
-    // const element = document.getElementById(elementId);
-    // if (!element) {
-    //   console.error('No editor container found');
-    //   return;
-    // }
 
     const script = document.createElement('script');
     script.type = 'module';
     script.src = scriptUrl;
     script.async = true;
-
-    // script.onload = () => {
-    //   loaded.set(initialLocale, scriptUrl);
-    //   window.cfInitVisualEditor(element, { initialLocale, initialComponentRegistry });
-    // };
 
     document.body.appendChild(script);
 
@@ -79,18 +28,6 @@ const VisualEditor: React.FC<{ elementId: string }> = ({ elementId }) => {
 
   return <div id={elementId} />;
 };
-
-// const VisualEditor = React.lazy(() =>
-//   loadRemoteScript(scriptUrl).then(() => {
-//     console.log('[SDK::DEBUG] script loaded from', scriptUrl);
-//     console.log('[SDK::DEBUG] window.cfVisualEditor:', window.cfVisualEditor);
-//     return typeof window.cfInitVisualEditor === 'function'
-//       ? {
-//           default: window.cfInitVisualEditor,
-//         }
-//       : Promise.reject(new Error('what the frick'));
-//   })
-// );
 
 type Props = {
   initialLocale: string;
