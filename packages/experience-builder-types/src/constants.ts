9649055def5455a1181880cce9407076fdb5d3b4
Adrian Meyer <8539634+primeinteger@users.noreply.github.com>
2023-12-13 10:34:40 -0700

    fix: add new constant VISUAL_EDITOR_CONTAINER_ID

--- packages/experience-builder-types/src/constants.ts
+++ packages/experience-builder-types/src/constants.ts
@@ -46,6 +46,7 @@ export const VISUAL_EDITOR_EVENTS = {
   Ready: 'cfVisualEditorReady',
 };
 
+export const VISUAL_EDITOR_CONTAINER_ID = 'cf-visual-editor';
 export const CONTENTFUL_SECTION_ID = 'contentful-section'; // TODO: remove me once all customers are using 2023-09-28 schema version
 export const CONTENTFUL_CONTAINER_ID = 'contentful-container';
 export const CONTENTFUL_SECTION_NAME = 'Section'; // TODO: remove me once all customers are using 2023-09-28 schema version
