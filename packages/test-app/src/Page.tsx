b5e6f4686e7be5b523a49b5f8dc2d18bfc1b6f8d
Chase Poirier <chase.poirier@contentful.com>
2024-01-11 12:54:33 -0700

    fix(visual-editor): enable dropzones on custom components if children=true

--- packages/test-app/src/Page.tsx
+++ packages/test-app/src/Page.tsx
@@ -1,9 +1,33 @@
 import { useParams } from 'react-router-dom';
 import '@contentful/experience-builder-components/styles.css';
 import './styles.css';
-import { ExperienceRoot, useFetchBySlug } from '@contentful/experience-builder';
+import { ExperienceRoot, defineComponents, useFetchBySlug } from '@contentful/experience-builder';
 import { useContentfulClient } from './hooks/useContentfulClient';
 import { useContentfulConfig } from './hooks/useContentfulConfig';
+import Duplex from './components/Duplex';
+
+defineComponents([
+  {
+    component: Duplex,
+    definition: {
+      id: 'duplex',
+      children: true,
+      name: 'Duplex',
+      category: 'Basic',
+      builtInStyles: ['cfMargin', 'cfPadding'],
+      variables: {
+        heading: {
+          displayName: 'Heading',
+          type: 'Text',
+          defaultValue: 'Lorem ipsum',
+        },
+      },
+    },
+    options: {
+      wrapComponent: false,
+    },
+  },
+]);
 
 export default function Page() {
   const { slug = 'homePage' } = useParams<{ slug: string }>();
---
+++ packages/test-app/src/components/Duplex.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+interface DuplexProps {
+  children: React.ReactNode;
+  heading: string;
+}
+
+const Duplex: React.FC<DuplexProps> = ({ children, heading, ...rest }) => {
+  return (
+    <div
+      {...rest}
+      style={{
+        width: '100vw',
+        display: 'flex',
+        alignItems: 'center',
+        padding: 50,
+        justifyContent: 'center',
+      }}>
+      <div
+        style={{
+          width: '100%',
+          gap: 75,
+          display: 'flex',
+          alignItems: 'center',
+          maxWidth: 1200,
+          flexDirection: 'row',
+        }}>
+        <div style={{ flex: 1 }}>
+          <h1
+            style={{
+              margin: 0,
+            }}>
+            {heading}
+          </h1>
+          {children}
+        </div>
+        <div
+          style={{
+            flex: 1,
+            backgroundColor: 'lightblue',
+            minHeight: 300,
+          }}>
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Duplex;
