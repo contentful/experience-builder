6ccea0f6e2b3e58852ee4e137ead35678831f84d
Chase Poirier <chase.poirier@contentful.com>
2024-01-11 11:24:08 -0700

    fix(visual-editor): canvas interactions flickering on changes [ALT-241] (#240)
    * fix: canvas interactions flickering on changes
    
    * fix: sync package-lock with next branch
    
    * fix(experience-builder-sdk): export ComponentDefintion (#243)
    
    * fix(visual-editor): properly handle when drag operations are canceled (#242)
    
    * fix(core): enable design tokens for text color style [ALT-249] (#244)
    
    * fix(pipeline): jobs not triggering on branches with '/' (#249)
    
    * chore(pipeline): remove logic for feat/native-visual-editor
    
    * fix(pipeline): jobs not triggering on branches with '/'
    
    * chore(visual-editor): change actions to enum, remove useless return statements
    
    * fix(visual-editor): forgotten change to enum
    
    ---------
    
    Co-authored-by: Ely Lucas <ely@meta-tek.net>
    Co-authored-by: Adrian Meyer <8539634+primeinteger@users.noreply.github.com>

--- packages/visual-editor/src/store/tree.ts
+++ packages/visual-editor/src/store/tree.ts
@@ -1,14 +1,37 @@
-import type { Breakpoint, CompositionTree } from '@contentful/experience-builder-core/types';
-import { ROOT_ID } from '@/types/constants';
+import type {
+  Breakpoint,
+  CompositionComponentNode,
+  CompositionTree,
+} from '@contentful/experience-builder-core/types';
+import { ROOT_ID, TreeAction } from '@/types/constants';
 import { create } from 'zustand';
+import { produce } from 'immer';
+import {
+  addChildNode,
+  removeChildNode,
+  reorderChildNode,
+  replaceNode,
+  updateNode,
+} from '@/utils/treeHelpers';
+import { getTreeDiffs } from '@/utils/getTreeDiff';
 
 export interface TreeStore {
   tree: CompositionTree;
   breakpoints: Breakpoint[];
   updateTree: (tree: CompositionTree) => void;
+  addChild: (
+    destinationIndex: number,
+    destinationParentId: string,
+    node: CompositionComponentNode
+  ) => void;
+  reorderChildren: (
+    destinationIndex: number,
+    destinationParentId: string,
+    sourceIndex: number
+  ) => void;
 }
 
-export const useTreeStore = create<TreeStore>((set) => ({
+export const useTreeStore = create<TreeStore>((set, get) => ({
   tree: {
     root: {
       children: [],
@@ -23,5 +46,70 @@ export const useTreeStore = create<TreeStore>((set) => ({
     },
   },
   breakpoints: [],
-  updateTree: (tree) => set({ tree, breakpoints: tree?.root?.data?.breakpoints || [] }),
+  updateTree: (tree) => {
+    const currentTree = get().tree;
+
+    /**
+     * If we simply update the tree as in:
+     *
+     * `state.tree = tree`
+     *
+     * we end up causing a lot of unnecesary rerenders which can lead to
+     * flickering of the component layout. Instead, we use this function
+     * to deteremine exactly which nodes in the tree changed and combined
+     * with immer, we end up updating only the changed nodes instead of
+     * rerendering the entire tree.
+     */
+    const treeDiff = getTreeDiffs({ ...currentTree.root }, { ...tree.root }, currentTree);
+
+    console.log('::DIFF', tree);
+
+    // The current and updated tree are the same, no tree update required.
+    if (!treeDiff.length) {
+      return;
+    }
+
+    set(
+      produce((state: TreeStore) => {
+        treeDiff.map((diff) => {
+          switch (diff.type) {
+            case TreeAction.ADD_NODE:
+              addChildNode(diff.indexToAdd, diff.parentNodeId, diff.nodeToAdd, state.tree.root);
+              break;
+            case TreeAction.REPLACE_NODE:
+              replaceNode(diff.originalId, diff.node, state.tree.root);
+              break;
+            case TreeAction.UPDATE_NODE:
+              updateNode(diff.nodeId, diff.node, state.tree.root);
+              break;
+            case TreeAction.REMOVE_NODE:
+              removeChildNode(diff.indexToRemove, diff.parentNodeId, state.tree.root);
+              break;
+            case TreeAction.MOVE_NODE:
+            case TreeAction.REORDER_NODE:
+              state.tree = tree;
+              break;
+            default:
+              break;
+          }
+        });
+
+        state.breakpoints = tree?.root?.data?.breakpoints || [];
+      })
+    );
+  },
+  addChild: (index, parentId, node) => {
+    set(
+      produce((state: TreeStore) => {
+        addChildNode(index, parentId, node, state.tree.root);
+      })
+    );
+  },
+  reorderChildren: (destinationIndex, destinationParentId, sourceIndex) => {
+    set(
+      produce((state: TreeStore) => {
+        reorderChildNode(sourceIndex, destinationIndex, destinationParentId, state.tree.root);
+      })
+    );
+  },
 }));
--- packages/visual-editor/src/types/constants.ts
+++ packages/visual-editor/src/types/constants.ts
@@ -8,3 +8,12 @@ export const DRAGGABLE_WIDTH = 92;
 export const ROOT_ID = 'root';
 
 export const builtInComponents = [CONTENTFUL_CONTAINER_ID, CONTENTFUL_SECTION_ID];
+
+export enum TreeAction {
+  REMOVE_NODE,
+  ADD_NODE,
+  MOVE_NODE,
+  UPDATE_NODE,
+  REORDER_NODE,
+  REPLACE_NODE,
+}
