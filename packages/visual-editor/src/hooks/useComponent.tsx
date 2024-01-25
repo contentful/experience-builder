8dcc65112fc3e068506c97216cac558657c68cb1
Thomas Kellermeier <thomas.kellermeier@contentful.com>
2024-01-25 12:20:05 +0100

    fix: use casting to overcome type clash

--- packages/visual-editor/src/hooks/useComponent.tsx
+++ packages/visual-editor/src/hooks/useComponent.tsx
@@ -68,7 +68,7 @@ export const useComponent = ({ node: rawNode, resolveDesignValue }: ComponentPar
   // Only pass editor props to built-in components
   const { editorMode, renderDropzone, ...componentProps } = props;
   const elementToRender = builtInComponents.includes(node.data.blockId || '') ? (
-    <ContentfulContainer {...props} />
+    <ContentfulContainer {...(props as React.ComponentProps<typeof ContentfulContainer>)} />
   ) : node.type === DESIGN_COMPONENT_NODE_TYPE || node.type === ASSEMBLY_NODE_TYPE ? (
     // Assembly.tsx requires renderDropzone and editorMode as well
     React.createElement(componentRegistration.component, props)
