048948810cd8906a8c41322a8211e45e08a2f21b
Sofia Margariti <sofia.margariti@contentful.com>
2024-02-06 12:59:31 +0000

    feat: first draft of schemas

---
+++ packages/validators/rollup.config.mjs
@@ -0,0 +1,28 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        dir: './dist',
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+    ],
+    external: [/node_modules/],
+  },
+  {
+    input: 'src/index.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+];
