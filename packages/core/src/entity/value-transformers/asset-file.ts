c4c66dfb85ea13d5674bd2069702d83f67400b12
Dimitry Kireyenkoff <dimitry.kireyenkoff@contentful.com>
2024-02-08 09:44:27 +0000

    fix: adds value-transformers

---
+++ packages/core/src/entity/value-transformers/asset-file.ts
@@ -0,0 +1,9 @@
+import { AssetFile } from 'contentful';
+
+export function transformAssetFileToUrl(
+  fieldValue: string | AssetFile | undefined
+): string | undefined {
+  return fieldValue && typeof fieldValue == 'object' && (fieldValue as AssetFile).url
+    ? (fieldValue as AssetFile).url
+    : (fieldValue as string | undefined);
+}
