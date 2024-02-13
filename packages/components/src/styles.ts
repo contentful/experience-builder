e201076515e8712c166a0e9104bc28e321f9aa0f
Chase Poirier <chase.poirier@contentful.com>
2024-02-13 14:50:57 -0700

    chore: refactor how styling is handled for built-ins

--- packages/components/src/styles.ts
+++ packages/components/src/styles.ts
@@ -1,5 +1,2 @@
 import './components/variables.css';
 import './components/Image/Image.css';
-import './components/Text/Text.css';
-import './components/Heading/Heading.css';
-import './components/RichText/RichText.css';
