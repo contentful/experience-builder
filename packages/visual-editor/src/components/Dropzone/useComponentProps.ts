6566f6427976f2539850941b53ae77690a38874e
Dimitry Kireyenkoff <dimitry.kireyenkoff@contentful.com>
2024-02-15 09:03:47 +0000

    fix: make idle cycle message go as console.debug

--- packages/visual-editor/src/components/Dropzone/useComponentProps.ts
+++ packages/visual-editor/src/components/Dropzone/useComponentProps.ts
@@ -90,8 +90,8 @@ export const useComponentProps = ({
           };
         } else if (variableMapping.type === 'BoundValue') {
           if (!areEntitiesFetched) {
-            console.warn(
-              `[exp-builder.sdk::Dropzone::useComponentProps::'BoundValue'] Idle-cycle: as entities are not fetched(areEntitiesFetched=${areEntitiesFetched}), we cannot resolve bound values for ${variableName} so we just resolve them to default values.`
+            console.debug(
+              `[exp-builder.sdk::useComponentProps] Idle-cycle: as entities are not fetched(areEntitiesFetched=${areEntitiesFetched}), we cannot resolve bound values for ${variableName} so we just resolve them to default values.`
             );
 
             // Just forcing default value (if we're in idle-cycle, entities are missing)
