4611258173f6dbe91ffd0a0398cd8ad2cc4ec908
Dimitry Kireyenkoff <dimitry.kireyenkoff@contentful.com>
2024-02-15 09:18:58 +0000

    fix: removing ugly cast

--- packages/core/src/utils/isLink.ts
+++ packages/core/src/utils/isLink.ts
@@ -1,5 +1,10 @@
 import { Asset, Entry, UnresolvedLink } from 'contentful';
 
 export const isLink = (
-  entity: UnresolvedLink<'Entry' | 'Asset'> | Entry | Asset
-): entity is UnresolvedLink<'Entry' | 'Asset'> => entity?.sys?.type === 'Link';
+  entity: UnresolvedLink<'Entry' | 'Asset'> | Entry | Asset | string
+): entity is UnresolvedLink<'Entry' | 'Asset'> => {
+  if (typeof entity === 'string') {
+    return false;
+  }
+  return entity?.sys?.type === 'Link';
+};
