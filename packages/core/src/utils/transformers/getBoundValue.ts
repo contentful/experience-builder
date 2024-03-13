0f23e3c233b36cbab418f29c2ffa05bea006900d
Ely Lucas <ely@meta-tek.net>
2024-03-13 16:16:24 -0600

    fix: updating getBoundValue to use new type

--- packages/core/src/utils/transformers/getBoundValue.ts
+++ packages/core/src/utils/transformers/getBoundValue.ts
@@ -1,11 +1,8 @@
-import { CompositionVariableValueType } from '@/types';
+import { PrimitiveValue } from '@/types';
 import { Asset, AssetFile, Entry } from 'contentful';
 import { get } from '../get';
 
-export const getBoundValue = (
-  entryOrAsset: Entry | Asset,
-  path: string,
-): CompositionVariableValueType => {
+export const getBoundValue = (entryOrAsset: Entry | Asset, path: string): PrimitiveValue => {
   const value = get<string>(entryOrAsset, path.split('/').slice(2, -1));
   return value && typeof value == 'object' && (value as AssetFile).url
     ? (value as AssetFile).url
