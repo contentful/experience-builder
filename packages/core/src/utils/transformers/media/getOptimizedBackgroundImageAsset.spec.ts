e8e0744c5a843f70946ff1a3971d74d3e6008db9
Adrian Meyer <8539634+primeinteger@users.noreply.github.com>
2024-03-19 09:09:27 -0600

    fix: update tests

--- packages/core/src/utils/transformers/media/getOptimizedBackgroundImageAsset.spec.ts
+++ packages/core/src/utils/transformers/media/getOptimizedBackgroundImageAsset.spec.ts
@@ -17,63 +17,81 @@ describe('transformImageAsset', () => {
   it('when width of container is not provided and image is larger than the max width, should return a srcSet with a basis width of 2000px', () => {
     file.details.image!.width = 4000;
     const result = getOptimizedBackgroundImageAsset(file, '');
-    expect(result.srcSet).toEqual([`url(${file.url}?w=2000) 1x`, `url(${file.url}?w=4000) 2x`]);
+    expect(result.srcSet).toEqual([
+      `url(https:${file.url}?w=2000) 1x`,
+      `url(https:${file.url}?w=4000) 2x`,
+    ]);
   });
 
   it('when width of container is not provided and image is larger than the max width, should return a url with a width of 4000px', () => {
     file.details.image!.width = 4000;
     const result = getOptimizedBackgroundImageAsset(file, '');
-    expect(result.url).toEqual('//images.contentful.com/abc/123/a1b2c3/myImage.jpg?w=4000');
+    expect(result.url).toEqual('https://images.contentful.com/abc/123/a1b2c3/myImage.jpg?w=4000');
   });
 
   it('when width of container is not provided and the image is smaller than the max width, should return a srcSet with a basis width of the image width, and 2x at same width', () => {
     file.details.image!.width = 1000;
     const result = getOptimizedBackgroundImageAsset(file, '');
-    expect(result.srcSet).toEqual([`url(${file.url}?w=1000) 1x`, `url(${file.url}?w=1000) 2x`]);
+    expect(result.srcSet).toEqual([
+      `url(https:${file.url}?w=1000) 1x`,
+      `url(https:${file.url}?w=1000) 2x`,
+    ]);
   });
 
   it('when width of container is not provided and the image is smaller than the max width, should return a url with a width of the image', () => {
     file.details.image!.width = 1000;
     const result = getOptimizedBackgroundImageAsset(file, '');
-    expect(result.url).toEqual('//images.contentful.com/abc/123/a1b2c3/myImage.jpg?w=1000');
+    expect(result.url).toEqual('https://images.contentful.com/abc/123/a1b2c3/myImage.jpg?w=1000');
   });
 
   it('when width of container is 1000px and image is larger than the max width, should return a srcSet with a basis width of 1000px', () => {
     file.details.image!.width = 4000;
     const result = getOptimizedBackgroundImageAsset(file, '1000px');
-    expect(result.srcSet).toEqual([`url(${file.url}?w=1000) 1x`, `url(${file.url}?w=2000) 2x`]);
+    expect(result.srcSet).toEqual([
+      `url(https:${file.url}?w=1000) 1x`,
+      `url(https:${file.url}?w=2000) 2x`,
+    ]);
   });
 
   it('when width of container is 1000px and image is larger than the max width, should return a url with a width of 2x the provided width', () => {
     file.details.image!.width = 4000;
     const result = getOptimizedBackgroundImageAsset(file, '1000px');
-    expect(result.url).toEqual('//images.contentful.com/abc/123/a1b2c3/myImage.jpg?w=2000');
+    expect(result.url).toEqual('https://images.contentful.com/abc/123/a1b2c3/myImage.jpg?w=2000');
   });
 
   it('when width of container is 1000px and image is smaller than the max width, should return a srcSet with a basis width of the image width, and 2x at same width', () => {
     file.details.image!.width = 1000;
     const result = getOptimizedBackgroundImageAsset(file, '1000px');
-    expect(result.srcSet).toEqual([`url(${file.url}?w=1000) 1x`, `url(${file.url}?w=1000) 2x`]);
+    expect(result.srcSet).toEqual([
+      `url(https:${file.url}?w=1000) 1x`,
+      `url(https:${file.url}?w=1000) 2x`,
+    ]);
   });
 
   it('when quality is 0, should return urls without quality', () => {
     file.details.image!.width = 4000;
     const result = getOptimizedBackgroundImageAsset(file, '', 0);
-    expect(result.srcSet).toEqual([`url(${file.url}?w=2000) 1x`, `url(${file.url}?w=4000) 2x`]);
+    expect(result.srcSet).toEqual([
+      `url(https:${file.url}?w=2000) 1x`,
+      `url(https:${file.url}?w=4000) 2x`,
+    ]);
   });
 
   it('when quality is 100, should return urls without quality', () => {
     file.details.image!.width = 4000;
     const result = getOptimizedBackgroundImageAsset(file, '', 100);
-    expect(result.srcSet).toEqual([`url(${file.url}?w=2000) 1x`, `url(${file.url}?w=4000) 2x`]);
+    expect(result.srcSet).toEqual([
+      `url(https:${file.url}?w=2000) 1x`,
+      `url(https:${file.url}?w=4000) 2x`,
+    ]);
   });
 
   it('when quality is provided, should return urls with quality', () => {
     file.details.image!.width = 4000;
     const result = getOptimizedBackgroundImageAsset(file, '', 80);
     expect(result.srcSet).toEqual([
-      `url(${file.url}?w=2000&q=80) 1x`,
-      `url(${file.url}?w=4000&q=80) 2x`,
+      `url(https:${file.url}?w=2000&q=80) 1x`,
+      `url(https:${file.url}?w=4000&q=80) 2x`,
     ]);
   });
 
@@ -81,8 +99,8 @@ describe('transformImageAsset', () => {
     file.details.image!.width = 4000;
     const result = getOptimizedBackgroundImageAsset(file, '', 80, 'webp');
     expect(result.srcSet).toEqual([
-      `url(${file.url}?w=2000&q=80&fm=webp) 1x`,
-      `url(${file.url}?w=4000&q=80&fm=webp) 2x`,
+      `url(https:${file.url}?w=2000&q=80&fm=webp) 1x`,
+      `url(https:${file.url}?w=4000&q=80&fm=webp) 2x`,
     ]);
   });
 });
--- packages/core/src/utils/transformers/media/getOptimizedImageUrl.spec.ts
+++ packages/core/src/utils/transformers/media/getOptimizedImageUrl.spec.ts
@@ -8,7 +8,7 @@ describe('getImageUrl', () => {
     const quality = 80;
     const format = 'webp';
     const result = getOptimizedImageUrl(url, width, quality, format);
-    expect(result).toBe('//example.com/image.jpg?w=200&q=80&fm=webp');
+    expect(result).toBe('https://example.com/image.jpg?w=200&q=80&fm=webp');
   });
 
   it('when quality is below 0, quality should not be added to the url', () => {
@@ -17,7 +17,7 @@ describe('getImageUrl', () => {
     const quality = -1;
     const format = 'webp';
     const result = getOptimizedImageUrl(url, width, quality, format);
-    expect(result).toBe('//example.com/image.jpg?w=200&fm=webp');
+    expect(result).toBe('https://example.com/image.jpg?w=200&fm=webp');
   });
 
   it('when quality is 0, quality should not be added to the url', () => {
@@ -26,7 +26,7 @@ describe('getImageUrl', () => {
     const quality = 0;
     const format = 'webp';
     const result = getOptimizedImageUrl(url, width, quality, format);
-    expect(result).toBe('//example.com/image.jpg?w=200&fm=webp');
+    expect(result).toBe('https://example.com/image.jpg?w=200&fm=webp');
   });
 
   it('when quality is 100, quality should not be added to the url', () => {
@@ -35,7 +35,7 @@ describe('getImageUrl', () => {
     const quality = 100;
     const format = 'webp';
     const result = getOptimizedImageUrl(url, width, quality, format);
-    expect(result).toBe('//example.com/image.jpg?w=200&fm=webp');
+    expect(result).toBe('https://example.com/image.jpg?w=200&fm=webp');
   });
 
   it('when quality is above 100, quality should not be added to the url', () => {
@@ -44,7 +44,7 @@ describe('getImageUrl', () => {
     const quality = 101;
     const format = 'webp';
     const result = getOptimizedImageUrl(url, width, quality, format);
-    expect(result).toBe('//example.com/image.jpg?w=200&fm=webp');
+    expect(result).toBe('https://example.com/image.jpg?w=200&fm=webp');
   });
 
   it('when format is not provided, format should not be added to the url', () => {
@@ -52,7 +52,7 @@ describe('getImageUrl', () => {
     const width = 200;
     const quality = 80;
     const result = getOptimizedImageUrl(url, width, quality);
-    expect(result).toBe('//example.com/image.jpg?w=200&q=80');
+    expect(result).toBe('https://example.com/image.jpg?w=200&q=80');
   });
 
   it('when width is not provided, width should not be added to the url', () => {
@@ -60,12 +60,12 @@ describe('getImageUrl', () => {
     const quality = 80;
     const format = 'webp';
     const result = getOptimizedImageUrl(url, 0, quality, format);
-    expect(result).toBe('//example.com/image.jpg?q=80&fm=webp');
+    expect(result).toBe('https://example.com/image.jpg?q=80&fm=webp');
   });
 
   it('when no optional parameters are provided, the url should not have any query string', () => {
     const url = '//example.com/image.jpg';
     const result = getOptimizedImageUrl(url);
-    expect(result).toBe('//example.com/image.jpg');
+    expect(result).toBe('https://example.com/image.jpg');
   });
 });
