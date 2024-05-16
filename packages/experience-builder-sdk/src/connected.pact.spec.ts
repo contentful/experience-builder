70a8024348edafc792c91fa7315ea88b1a7fafe0
Sofia Margariti <sofia.margariti@contentful.com>
2024-05-16 13:41:44 +0100

    test: add some comments

--- packages/experience-builder-sdk/src/connected.pact.spec.ts
+++ packages/experience-builder-sdk/src/connected.pact.spec.ts
@@ -7,6 +7,7 @@ import { OUTGOING_EVENTS } from '@contentful/experiences-core/constants';
 const messageProvider = () => {
   let message;
   jest.spyOn(window, 'postMessage').mockImplementation((msg) => {
+    // We need to only intercept the OUTGOING_EVENTS.Connected event, sendConnectedEventWithRegisteredComponents also sends OUTGOING_EVENTS.DesignTokens
     if (msg.eventType === OUTGOING_EVENTS.Connected) {
       message = msg;
     }
@@ -20,13 +21,14 @@ const messageProvider = () => {
 describe('Pact Verification', () => {
   // 2 Pact setup
   const p = new MessageProviderPact({
+    // For convinence we using the pacts folder that is created in user_interface. In reality pacts would be loaded from the pact broker (e.g. PactFlow)
     pactUrls: [path.resolve(process.cwd(), '../../../user_interface/pacts')],
     messageProviders: {
       'a connected message': messageProvider,
     },
     logLevel: 'debug',
     provider: 'ExperiencesSDKProvider',
-    providerVersion: '1.0.0',
+    providerVersion: '1.3.0',
   });
 
   it('verifies the interactions', () => {
