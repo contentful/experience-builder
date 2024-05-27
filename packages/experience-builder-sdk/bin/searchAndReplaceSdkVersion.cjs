45aa880462a58f48fbd3312c346c80aa17596086
Ely Lucas <ely@meta-tek.net>
2024-05-27 14:05:36 -0600

    Update searchAndReplaceSdkVersion.cjs

--- packages/experience-builder-sdk/bin/searchAndReplaceSdkVersion.cjs
+++ packages/experience-builder-sdk/bin/searchAndReplaceSdkVersion.cjs
@@ -6,7 +6,6 @@ const { glob } = require('glob');
 // The function `searchAndReplaceSdkVersion` is used to find instances of the SDK version in the built dist folder
 // and replace them with the current version. This is typically used during the build process to ensure
 // that the correct version number is reflected in the final build.
-searchAndReplaceSdkVersion;
 searchAndReplaceSdkVersion('./dist/**/*.{js,ts,map,d.ts}', process.env.npm_package_version);
 
 async function searchAndReplaceSdkVersion(pattern, replacement) {
