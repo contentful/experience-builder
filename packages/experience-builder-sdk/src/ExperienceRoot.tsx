86e67caa8eea5f94920192cd0e06bcb241b63bab
Ely Lucas <ely@meta-tek.net>
2024-06-23 16:19:17 -0600

    fix: allow experiences-sdk-react components/hooks to be imported in NextJS server components

--- packages/experience-builder-sdk/src/ExperienceRoot.tsx
+++ packages/experience-builder-sdk/src/ExperienceRoot.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 import { VisualEditorMode, validateExperienceBuilderConfig } from '@contentful/experiences-core';
 import { EntityStore } from '@contentful/experiences-core';
