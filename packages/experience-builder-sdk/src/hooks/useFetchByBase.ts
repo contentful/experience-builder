ee5ef2b7f76f64868827ed2d5fa2852656f74ceb
Ely Lucas <ely@meta-tek.net>
2024-06-25 12:49:32 -0600

    fix: allow experiences-sdk-react components/hooks to be imported in NextJS server components (#667)
    * fix: allow experiences-sdk-react components/hooks to be imported in NextJS server components
    
    * fix: allow passing in JSON experience to ExperienceRoot
    
    * chore: add null to experience param union
    
    * fix: cast Experience to expected types

--- packages/experience-builder-sdk/src/hooks/useFetchByBase.ts
+++ packages/experience-builder-sdk/src/hooks/useFetchByBase.ts
@@ -1,3 +1,4 @@
+'use client';
 import { useEffect, useState } from 'react';
 import { EntityStore } from '@contentful/experiences-core';
 import type { Experience } from '@contentful/experiences-core/types';
