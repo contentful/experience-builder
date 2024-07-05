28aa6240d9feab33a804bb61b5498c6bc283fadb
Sofia Margariti <sofia.margariti@contentful.com>
2024-07-05 17:01:20 +0100

    test: add pact verification for componentSelected events

--- packages/visual-editor/src/components/DraggableBlock/EditorBlock.spec.tsx
+++ packages/visual-editor/src/components/DraggableBlock/EditorBlock.spec.tsx
@@ -1,122 +1,56 @@
-import React from 'react';
+import React, { act } from 'react';
 import { vi } from 'vitest';
-import { act, fireEvent, render, renderHook, screen } from '@testing-library/react';
-import { ComponentRegistration, ExperienceTree } from '@contentful/experiences-core/types';
+import { fireEvent, render, renderHook, screen } from '@testing-library/react';
 import { addComponentRegistration } from '../../store/registries';
 import { VisualEditorRoot } from '../VisualEditorRoot';
 import { createExperience } from '@contentful/experiences-core';
-import { experienceEntry } from '../../../test/__fixtures__/composition';
-import { entries, assets } from '../../../test/__fixtures__/entities';
+import { experienceEntry, tree, componentRegistrations } from '../../../test/__fixtures__';
 import { Entry } from 'contentful';
 import { useTreeStore } from '../../store/tree';
 import { OUTGOING_EVENTS } from '@contentful/experiences-core/constants';
 
-const componentRegistration1: ComponentRegistration = {
-  definition: {
-    id: 'component-1',
-    name: 'Component 1',
-    variables: {},
-  },
-  component: () => <div>Block 1</div>,
-  options: {
-    wrapComponent: false,
-  },
-};
-const componentRegistration2: ComponentRegistration = {
-  definition: {
-    id: 'component-2',
-    name: 'Component 2',
-    variables: {},
-  },
-  component: () => <div>Block 2</div>,
-  options: {
-    wrapComponent: false,
-  },
-};
-
-const tree: ExperienceTree = {
-  root: {
-    children: [
-      {
-        children: [],
-        type: 'block',
-        data: {
-          breakpoints: [],
-          dataSource: {},
-          id: 'component-1',
-          props: {},
-          unboundValues: {},
-          blockId: 'component-1',
-        },
-      },
-      {
-        children: [],
-        type: 'block',
-        data: {
-          breakpoints: [],
-          dataSource: {},
-          id: 'component-2',
-          props: {},
-          unboundValues: {},
-          blockId: 'component-2',
-        },
-      },
-    ],
-    type: 'root',
-    data: {
-      breakpoints: [],
-      dataSource: {},
-      id: 'root',
-      props: {},
-      unboundValues: {},
-    },
-  },
-};
+console.log('experienceEntry', experienceEntry);
 
 const experience = createExperience({
   experienceEntry: experienceEntry as unknown as Entry,
-  referencedAssets: assets,
-  referencedEntries: entries,
+  referencedAssets: [],
+  referencedEntries: [],
   locale: 'en-US',
 });
-let sendMessageSpy;
-let message;
-describe('EditorBlock', () => {
-  beforeEach(() => {
-    sendMessageSpy = vi.spyOn(window, 'postMessage').mockImplementation((msg) => {
-      if (msg.eventType === OUTGOING_EVENTS.ComponentSelected) {
-        message = msg;
-      }
-    });
+vi.mock(
+  '../../hooks/useInitializeEditor',
+  vi.fn(() => ({ useInitializeEditor: () => true })),
+);
+describe('VisualEditorRoot', () => {
+  it('should render the editor', () => {
+    addComponentRegistration(componentRegistrations[0]);
+    addComponentRegistration(componentRegistrations[1]);
+
     const { result } = renderHook(() =>
       useTreeStore((state) => ({
         updateTree: state.updateTree,
       })),
     );
-    addComponentRegistration(componentRegistration1);
-    addComponentRegistration(componentRegistration2);
     act(() => result.current.updateTree(tree));
-    vi.mock(
-      '../../hooks/useInitializeEditor',
-      vi.fn(() => ({ useInitializeEditor: () => true })),
-    );
-  });
 
-  const renderComponent = () => render(<VisualEditorRoot experience={experience} />);
-
-  it('should render EditorBlock with props', () => {
-    renderComponent();
-    const el = screen.getByText('Block 1');
-    fireEvent.click(el);
-    expect(message).toEqual({
-      eventType: OUTGOING_EVENTS.ComponentSelected,
-      payload: {
-        assembly: undefined,
-        nodeId: 'component-1',
-      },
-      source: 'customer-app',
-    });
-
+    render(<VisualEditorRoot experience={experience} />);
     screen.debug();
+    const el1 = screen.getByText('Block 1');
+    fireEvent.click(el1);
+
+    addComponentRegistration(componentRegistrations[0]);
+    addComponentRegistration(componentRegistrations[1]);
+
+    const result2 = renderHook(() =>
+      useTreeStore((state) => ({
+        updateTree: state.updateTree,
+      })),
+    );
+    act(() => result2.result.current.updateTree(tree));
+
+    render(<VisualEditorRoot experience={experience} />);
+    screen.debug();
+    const el2 = screen.getByText('Block 2');
+    fireEvent.click(el2);
   });
 });
--- packages/visual-editor/test/verification.pact.spec.tsx
+++ packages/visual-editor/test/verification.pact.spec.tsx
@@ -1,21 +1,64 @@
 import React, { act } from 'react';
-import { fireEvent, render, renderHook, screen } from '@testing-library/react';
-import { vitest } from 'vitest';
+import { cleanup, fireEvent, render, renderHook, screen } from '@testing-library/react';
+import { vi } from 'vitest';
 import { useEditorSubscriber } from '../src/hooks/useEditorSubscriber';
 import { OUTGOING_EVENTS } from '@contentful/experiences-core/constants';
 import { MessageProviderPact } from '@pact-foundation/pact';
 import path from 'path';
 import { VisualEditorRoot } from '../src/components/VisualEditorRoot';
 
-import { EntityStore, defineBreakpoints, defineDesignTokens } from '@contentful/experiences-core';
+import {
+  EntityStore,
+  createExperience,
+  defineBreakpoints,
+  defineDesignTokens,
+} from '@contentful/experiences-core';
+import { addComponentRegistration } from '../src/store/registries';
 import {
   sendConnectedEventWithRegisteredComponents,
   sendRegisteredComponentsMessage,
 } from '@contentful/experiences-sdk-react/src/core/componentRegistry';
 import { Experience, OutgoingEvent } from '@contentful/experiences-core/types';
 import { RootRenderer } from '../src/components/RootRenderer/RootRenderer';
-import { DNDProvider } from '../src/components/RootRenderer/DNDProvider';
-import TestDNDContainer from '../src/components/RootRenderer/TestDNDContainer';
+import { componentRegistrations, experienceEntry, tree } from './__fixtures__';
+import { Entry } from 'contentful';
+import { useTreeStore } from '../src/store/tree';
+
+vi.mock(
+  '../src/hooks/useInitializeEditor',
+  vi.fn(() => ({ useInitializeEditor: () => true })),
+);
+
+const experience = createExperience({
+  experienceEntry: experienceEntry as unknown as Entry,
+  referencedAssets: [],
+  referencedEntries: [],
+  locale: 'en-US',
+});
+
+const InteractionIds = {
+  ConnectedInterationId: 'connected-event',
+  DesignTokensInterationId: 'design-tokens-event',
+  RegisteredBreakpointsInteractionId: 'registered-breakpoints-event',
+  ComponentSelectedInteractionId: 'component-selected-event',
+  MouseMoveInteractionId: 'mouse-move-event',
+  NewHoveredElementInteractionId: 'new-hovered-element-event',
+  AssemblyComponentSelectedInteractionId: 'assembly-component-selected-event',
+  RegisteredComponentsInteractionId: 'registered-components-event',
+  RequestComponentTreeUpdateInteractionId: 'request-component-tree-update-event',
+  ComponentDragCanceledInteractionId: 'component-drag-canceled-event',
+  ComponentDroppedInteractionId: 'component-dropped-event',
+  ComponentMovedInteractionId: 'component-moved-event',
+  CanvasReloadInteractionId: 'canvas-reload-event',
+  CanvasScrollInteractionId: 'canvas-scroll-event',
+  CanvasErrorInteractionId: 'canvas-error-event',
+  UpdateSelectedComponentCoordinatesInteractionId: 'update-selected-component-coordinates-event',
+  ComponentMoveStartedInteractionId: 'component-move-started-event',
+  ComponentMoveEndedInteractionId: 'component-move-ended-event',
+  OutsideCanvasClickInteractionId: 'outside-canvas-click-event',
+  SDKFeaturesInteractionId: 'sdk-features-event',
+  RequestEntitiesInteractionId: 'requested-entities-event',
+};
 
 const requestComponentTreeUpdateMessageProvider = () => {
   renderHook(() => useEditorSubscriber());
@@ -24,30 +67,6 @@ const requestComponentTreeUpdateMessageProvider = () => {
 const connectedMessageProvider = () => {
   sendConnectedEventWithRegisteredComponents();
 };
-
-export enum InteractionIds {
-  ConnectedInterationId = 'connected-event',
-  DesignTokensInterationId = 'design-tokens-event',
-  RegisteredBreakpointsInteractionId = 'registered-breakpoints-event',
-  ComponentSelectedInteractionId = 'component-selected-event',
-  MouseMoveInteractionId = 'mouse-move-event',
-  NewHoveredElementInteractionId = 'new-hovered-element-event',
-  AssemblyComponentSelectedInteractionId = 'assembly-component-selected-event',
-  RegisteredComponentsInteractionId = 'registered-components-event',
-  RequestComponentTreeUpdateInteractionId = 'request-component-tree-update-event',
-  ComponentDragCanceledInteractionId = 'component-drag-canceled-event',
-  ComponentDroppedInteractionId = 'component-dropped-event',
-  ComponentMovedInteractionId = 'component-moved-event',
-  CanvasReloadInteractionId = 'canvas-reload-event',
-  CanvasScrollInteractionId = 'canvas-scroll-event',
-  CanvasErrorInteractionId = 'canvas-error-event',
-  UpdateSelectedComponentCoordinatesInteractionId = 'update-selected-component-coordinates-event',
-  ComponentMoveStartedInteractionId = 'component-move-started-event',
-  ComponentMoveEndedInteractionId = 'component-move-ended-event',
-  OutsideCanvasClickInteractionId = 'outside-canvas-click-event',
-  SDKFeaturesInteractionId = 'sdk-features-event',
-  RequestEntitiesInteractionId = 'requested-entities-event',
-}
 
 const designTokensMessageProvider = () => {
   defineDesignTokens({
@@ -74,19 +93,17 @@ const breakpointsMessageProvider = () => {
   sendConnectedEventWithRegisteredComponents();
 };
 
-const componentSelectedMessageProvider = () => {};
-const assemblyComponentSelectedMessageProvider = () => {};
 const componentsRegisteredMessageProvider = () => {
   sendRegisteredComponentsMessage();
 };
 const mouseMoveMessageProvider = () => {
-  render(<VisualEditorRoot experience={vitest.fn() as Experience<EntityStore>} />);
+  render(<VisualEditorRoot experience={vi.fn() as Experience<EntityStore>} />);
 };
 
 const messageProviderWrapper = (messageProvider: () => any, eventType: OutgoingEvent) => {
   return () => {
     let message;
-    const postMessageSpy = vitest.spyOn(window, 'postMessage').mockImplementation((msg) => {
+    const postMessageSpy = vi.spyOn(window, 'postMessage').mockImplementation((msg) => {
       // We need to only intercept the OUTGOING_EVENTS.Connected event,
       // sendConnectedEventWithRegisteredComponents also sends OUTGOING_EVENTS.DesignTokens
       if (msg.eventType === eventType) {
@@ -102,7 +119,7 @@ const messageProviderWrapper = (messageProvider: () => any, eventType: OutgoingE
 };
 
 const newHoveredElementMessageProvider = () => {
-  render(<VisualEditorRoot experience={vitest.fn() as Experience<EntityStore>} />);
+  render(<VisualEditorRoot experience={vi.fn() as Experience<EntityStore>} />);
 };
 
 const outsideCanvasClickMessageProvider = () => {
@@ -116,17 +133,50 @@ const componentMoveStartedMessageProvider = () => {
 };
 
 const componentMoveEndedMessageProvider = () => {
-  render(<DNDProvider isTestRunOverride={true}>{}</DNDProvider>);
-  const testContainer = document.querySelector('[data-test-id="dnd-context-substitute"]');
-  testContainer && fireEvent.mouseDown(testContainer);
-  testContainer && fireEvent.mouseMove(testContainer, { clientX: 100, clientY: 100 });
-  const event = new CustomEvent('dragStart');
-  testContainer && testContainer.dispatchEvent(event);
-  console.log('div: ', testContainer);
-  // if (div) {
-  //   fireEvent.mouseDown(div);
-  // }
-  screen.debug();
+  // render(<DNDProvider isTestRunOverride={true}>{ }</DNDProvider>);
+  // const testContainer = document.querySelector('[data-test-id="dnd-context-substitute"]');
+  // testContainer && fireEvent.mouseDown(testContainer);
+  // testContainer && fireEvent.mouseMove(testContainer, { clientX: 100, clientY: 100 });
+  // const event = new CustomEvent('dragStart');
+  // testContainer && testContainer.dispatchEvent(event);
+  // console.log('div: ', testContainer);
+  // // if (div) {
+  // //   fireEvent.mouseDown(div);
+  // // }
+  // screen.debug();
+};
+
+const componentSelectedMessageProvider = () => {
+  addComponentRegistration(componentRegistrations[0]);
+  addComponentRegistration(componentRegistrations[1]);
+
+  const { result } = renderHook(() =>
+    useTreeStore((state) => ({
+      updateTree: state.updateTree,
+    })),
+  );
+  act(() => result.current.updateTree(tree));
+
+  render(<VisualEditorRoot experience={experience} />);
+  const el = screen.getByText('Block 1');
+  fireEvent.click(el);
+  cleanup();
+};
+const assemblyComponentSelectedMessageProvider = () => {
+  addComponentRegistration(componentRegistrations[0]);
+  addComponentRegistration(componentRegistrations[1]);
+
+  const { result } = renderHook(() =>
+    useTreeStore((state) => ({
+      updateTree: state.updateTree,
+    })),
+  );
+  act(() => result.current.updateTree(tree));
+
+  render(<VisualEditorRoot experience={experience} />);
+  const el = screen.getByText('Block 2');
+  fireEvent.click(el);
+  cleanup();
 };
 
 describe('Pact Verification', () => {
