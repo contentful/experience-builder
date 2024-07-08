4f3163a1cbbf79998b373849bafae1f48e997dbe
Sofia Margariti <sofia.margariti@contentful.com>
2024-07-08 14:46:31 +0100

    test: add verification for other events

--- packages/visual-editor/test/verification.pact.spec.tsx
+++ packages/visual-editor/test/verification.pact.spec.tsx
@@ -1,14 +1,13 @@
 import React, { act } from 'react';
-import { cleanup, fireEvent, render, renderHook, screen } from '@testing-library/react';
 import { vi } from 'vitest';
+import { cleanup, fireEvent, render, renderHook, screen } from '@testing-library/react';
 import { useEditorSubscriber } from '../src/hooks/useEditorSubscriber';
-import { OUTGOING_EVENTS } from '@contentful/experiences-core/constants';
+import { INCOMING_EVENTS, OUTGOING_EVENTS } from '@contentful/experiences-core/constants';
 import { MessageProviderPact } from '@pact-foundation/pact';
 import path from 'path';
 import { VisualEditorRoot } from '../src/components/VisualEditorRoot';
-
 import {
-  EntityStore,
+  EditorModeEntityStore,
   createExperience,
   defineBreakpoints,
   defineDesignTokens,
@@ -18,11 +17,19 @@ import {
   sendConnectedEventWithRegisteredComponents,
   sendRegisteredComponentsMessage,
 } from '@contentful/experiences-sdk-react/src/core/componentRegistry';
-import { Experience, OutgoingEvent } from '@contentful/experiences-core/types';
+import { useDetectEditorMode } from '@contentful/experiences-sdk-react/src/hooks/useDetectEditorMode';
+import { OutgoingEvent } from '@contentful/experiences-core/types';
 import { RootRenderer } from '../src/components/RootRenderer/RootRenderer';
-import { componentRegistrations, experienceEntry, tree } from './__fixtures__';
+import { assets, componentRegistrations, entries, experienceEntry, tree } from './__fixtures__';
 import { Entry } from 'contentful';
 import { useTreeStore } from '../src/store/tree';
+import { sendSelectedComponentCoordinates } from '../src/communication/sendSelectedComponentCoordinates';
+import { DropResult } from '@hello-pangea/dnd';
+import useCanvasInteractions from '../src/hooks/useCanvasInteractions';
+import SimulateDnD from '../src/utils/simulateDnD';
+import { ErrorBoundary } from '../../experience-builder-sdk/src/components/ErrorBoundary';
+
+const PACT_PROVIDER = 'ExperiencesSDKProvider';
 
 vi.mock(
   '../src/hooks/useInitializeEditor',
@@ -41,27 +48,44 @@ const InteractionIds = {
   DesignTokensInterationId: 'design-tokens-event',
   RegisteredBreakpointsInteractionId: 'registered-breakpoints-event',
   ComponentSelectedInteractionId: 'component-selected-event',
+  AssemblyComponentSelectedInteractionId: 'assembly-component-selected-event',
   MouseMoveInteractionId: 'mouse-move-event',
   NewHoveredElementInteractionId: 'new-hovered-element-event',
-  AssemblyComponentSelectedInteractionId: 'assembly-component-selected-event',
   RegisteredComponentsInteractionId: 'registered-components-event',
   RequestComponentTreeUpdateInteractionId: 'request-component-tree-update-event',
   ComponentDragCanceledInteractionId: 'component-drag-canceled-event',
   ComponentDroppedInteractionId: 'component-dropped-event',
   ComponentMovedInteractionId: 'component-moved-event',
-  CanvasReloadInteractionId: 'canvas-reload-event',
   CanvasScrollInteractionId: 'canvas-scroll-event',
   CanvasErrorInteractionId: 'canvas-error-event',
   UpdateSelectedComponentCoordinatesInteractionId: 'update-selected-component-coordinates-event',
-  ComponentMoveStartedInteractionId: 'component-move-started-event',
-  ComponentMoveEndedInteractionId: 'component-move-ended-event',
   OutsideCanvasClickInteractionId: 'outside-canvas-click-event',
   SDKFeaturesInteractionId: 'sdk-features-event',
   RequestEntitiesInteractionId: 'requested-entities-event',
 };
 
-const requestComponentTreeUpdateMessageProvider = () => {
-  renderHook(() => useEditorSubscriber());
+// wrapper for each message provider, spies on the `window.postMessage` method for the specified event,
+// intercepts and returns the message being sent
+const messageProviderWrapper = (messageProvider: () => any, eventType: OutgoingEvent) => {
+  return () => {
+    let message;
+    const postMessageSpy = vi.spyOn(window, 'postMessage').mockImplementation((msg) => {
+      // We need to only intercept the OUTGOING_EVENTS.Connected event,
+      // sendConnectedEventWithRegisteredComponents also sends OUTGOING_EVENTS.DesignTokens
+      if (msg.eventType === eventType) {
+        message = msg;
+      }
+    });
+
+    messageProvider();
+
+    postMessageSpy.mockRestore();
+
+    // reset components rendered in the test
+    cleanup();
+
+    return message;
+  };
 };
 
 const connectedMessageProvider = () => {
@@ -93,33 +117,55 @@ const breakpointsMessageProvider = () => {
   sendConnectedEventWithRegisteredComponents();
 };
 
-const componentsRegisteredMessageProvider = () => {
-  sendRegisteredComponentsMessage();
+const componentSelectedMessageProvider = () => {
+  renderEditorRoot();
+
+  const el = screen.getByText('Block 1');
+  fireEvent.click(el);
 };
+
+const assemblyComponentSelectedMessageProvider = () => {
+  renderEditorRoot();
+
+  const el = screen.getByText('Block 2');
+  fireEvent.click(el);
+  cleanup();
+};
+
 const mouseMoveMessageProvider = () => {
-  render(<VisualEditorRoot experience={vi.fn() as Experience<EntityStore>} />);
-};
+  renderEditorRoot();
 
-const messageProviderWrapper = (messageProvider: () => any, eventType: OutgoingEvent) => {
-  return () => {
-    let message;
-    const postMessageSpy = vi.spyOn(window, 'postMessage').mockImplementation((msg) => {
-      // We need to only intercept the OUTGOING_EVENTS.Connected event,
-      // sendConnectedEventWithRegisteredComponents also sends OUTGOING_EVENTS.DesignTokens
-      if (msg.eventType === eventType) {
-        message = msg;
-      }
-    });
-    messageProvider();
-
-    postMessageSpy.mockRestore();
-
-    return message;
-  };
+  const el = screen.getByText('Block 1');
+  SimulateDnD.updateIsDragging(true);
+  fireEvent.mouseMove(el, { clientX: 100, clientY: 100 });
 };
 
 const newHoveredElementMessageProvider = () => {
-  render(<VisualEditorRoot experience={vi.fn() as Experience<EntityStore>} />);
+  renderEditorRoot();
+
+  const el = screen.getByText('Block 1');
+  fireEvent.mouseOver(el);
+};
+
+const componentsRegisteredMessageProvider = () => {
+  sendRegisteredComponentsMessage();
+};
+
+const requestComponentTreeUpdateMessageProvider = () => {
+  renderHook(() => useEditorSubscriber());
+};
+
+const canvasScrollMessageProvider = () => {
+  renderHook(() => useEditorSubscriber());
+  window.dispatchEvent(new CustomEvent('scroll'));
+};
+
+const updateSelectedComponentCoordinatesMessageProvider = () => {
+  renderEditorRoot();
+
+  const el = screen.getByText('Block 1');
+  fireEvent.click(el);
+  sendSelectedComponentCoordinates(tree.root.children[0].data.id);
 };
 
 const outsideCanvasClickMessageProvider = () => {
@@ -127,56 +173,92 @@ const outsideCanvasClickMessageProvider = () => {
   fireEvent.click(document.body);
 };
 
-const componentMoveStartedMessageProvider = () => {
-  render(<RootRenderer />);
-  fireEvent.mouseDown(document.body);
-};
-
-const componentMoveEndedMessageProvider = () => {
-  // render(<DNDProvider isTestRunOverride={true}>{ }</DNDProvider>);
-  // const testContainer = document.querySelector('[data-test-id="dnd-context-substitute"]');
-  // testContainer && fireEvent.mouseDown(testContainer);
-  // testContainer && fireEvent.mouseMove(testContainer, { clientX: 100, clientY: 100 });
-  // const event = new CustomEvent('dragStart');
-  // testContainer && testContainer.dispatchEvent(event);
-  // console.log('div: ', testContainer);
-  // // if (div) {
-  // //   fireEvent.mouseDown(div);
-  // // }
-  // screen.debug();
-};
-
-const componentSelectedMessageProvider = () => {
-  addComponentRegistration(componentRegistrations[0]);
-  addComponentRegistration(componentRegistrations[1]);
-
-  const { result } = renderHook(() =>
-    useTreeStore((state) => ({
-      updateTree: state.updateTree,
-    })),
+const sdkFeaturesMessageProvider = () => {
+  renderHook(() => useDetectEditorMode());
+  void window.dispatchEvent(
+    new MessageEvent('message', {
+      data: {
+        eventType: INCOMING_EVENTS.RequestEditorMode,
+        source: 'composability-app',
+        origin: '',
+      },
+    }),
   );
-  act(() => result.current.updateTree(tree));
-
-  render(<VisualEditorRoot experience={experience} />);
-  const el = screen.getByText('Block 1');
-  fireEvent.click(el);
-  cleanup();
 };
-const assemblyComponentSelectedMessageProvider = () => {
-  addComponentRegistration(componentRegistrations[0]);
-  addComponentRegistration(componentRegistrations[1]);
 
-  const { result } = renderHook(() =>
-    useTreeStore((state) => ({
-      updateTree: state.updateTree,
-    })),
+const requestEntitiesMessageProvider = async () => {
+  const store = new EditorModeEntityStore({ locale: 'en-US', entities: entries });
+  const entryIds = entries.map((entry) => entry.sys.id);
+  const assetIds = assets.map((asset) => asset.sys.id);
+
+  store.fetchEntities({ missingEntryIds: entryIds, missingAssetIds: assetIds, skipCache: true });
+};
+
+const componentMovedMessageProvider = () => {
+  const { result } = renderHook(() => useCanvasInteractions());
+  const droppedItem: DropResult = {
+    destination: {
+      droppableId: 'root',
+      index: 0,
+    },
+    draggableId: 'draggable',
+    reason: 'DROP',
+    mode: 'FLUID',
+    combine: null,
+    type: 'TYPE',
+    source: {
+      droppableId: 'root',
+      index: 1,
+    },
+  };
+  act(() => result.current.onMoveComponent(droppedItem));
+};
+const componentDroppedMessageProvider = () => {
+  const { result } = renderHook(() => useCanvasInteractions());
+  const droppedItem: DropResult = {
+    destination: {
+      droppableId: 'root',
+      index: 0,
+    },
+    draggableId: 'draggable',
+    reason: 'DROP',
+    mode: 'FLUID',
+    combine: null,
+    type: 'TYPE',
+    source: {
+      droppableId: 'component-list',
+      index: 1,
+    },
+  };
+  act(() => result.current.onAddComponent(droppedItem));
+};
+
+const canvasErrorMessageProvider = () => {
+  let message;
+  const postMessageSpy = vi.spyOn(window, 'postMessage').mockImplementation((msg) => {
+    if (msg.eventType === OUTGOING_EVENTS.CanvasError) {
+      message = msg;
+    }
+  });
+
+  const TestComponent = () => {
+    throw new Error('Test error');
+  };
+  render(
+    <ErrorBoundary>
+      <TestComponent />
+    </ErrorBoundary>,
   );
-  act(() => result.current.updateTree(tree));
 
-  render(<VisualEditorRoot experience={experience} />);
-  const el = screen.getByText('Block 2');
-  fireEvent.click(el);
+  // stringify error in order to be able to compare it via the pact payload matcher
+  message.payload = JSON.parse(
+    JSON.stringify(message.payload, Object.getOwnPropertyNames(message.payload)),
+  );
+
+  postMessageSpy.mockRestore();
   cleanup();
+
+  return message;
 };
 
 describe('Pact Verification', () => {
@@ -226,57 +308,37 @@ describe('Pact Verification', () => {
         newHoveredElementMessageProvider,
         OUTGOING_EVENTS.NewHoveredElement,
       ),
-      [InteractionIds.ComponentDragCanceledInteractionId]: messageProviderWrapper(
-        () => {},
-        OUTGOING_EVENTS.ComponentDragCanceled,
-      ),
       [InteractionIds.ComponentDroppedInteractionId]: messageProviderWrapper(
-        () => {},
+        componentDroppedMessageProvider,
         OUTGOING_EVENTS.ComponentDropped,
       ),
       [InteractionIds.ComponentMovedInteractionId]: messageProviderWrapper(
-        () => {},
+        componentMovedMessageProvider,
         OUTGOING_EVENTS.ComponentMoved,
       ),
-      [InteractionIds.CanvasReloadInteractionId]: messageProviderWrapper(
-        () => {},
-        OUTGOING_EVENTS.CanvasReload,
-      ),
       [InteractionIds.CanvasScrollInteractionId]: messageProviderWrapper(
-        () => {},
+        canvasScrollMessageProvider,
         OUTGOING_EVENTS.CanvasScroll,
       ),
-      [InteractionIds.CanvasErrorInteractionId]: messageProviderWrapper(
-        () => {},
-        OUTGOING_EVENTS.CanvasError,
-      ),
+      [InteractionIds.CanvasErrorInteractionId]: canvasErrorMessageProvider,
       [InteractionIds.UpdateSelectedComponentCoordinatesInteractionId]: messageProviderWrapper(
-        () => {},
+        updateSelectedComponentCoordinatesMessageProvider,
         OUTGOING_EVENTS.UpdateSelectedComponentCoordinates,
-      ),
-      [InteractionIds.ComponentMoveStartedInteractionId]: messageProviderWrapper(
-        componentMoveStartedMessageProvider,
-        OUTGOING_EVENTS.ComponentMoveStarted,
-      ),
-      [InteractionIds.ComponentMoveEndedInteractionId]: messageProviderWrapper(
-        componentMoveEndedMessageProvider,
-        OUTGOING_EVENTS.ComponentMoveEnded,
       ),
       [InteractionIds.OutsideCanvasClickInteractionId]: messageProviderWrapper(
         outsideCanvasClickMessageProvider,
         OUTGOING_EVENTS.OutsideCanvasClick,
       ),
       [InteractionIds.SDKFeaturesInteractionId]: messageProviderWrapper(
-        () => {},
+        sdkFeaturesMessageProvider,
         OUTGOING_EVENTS.SDKFeatures,
       ),
       [InteractionIds.RequestEntitiesInteractionId]: messageProviderWrapper(
-        () => {},
+        requestEntitiesMessageProvider,
         OUTGOING_EVENTS.RequestEntities,
       ),
     },
-    //logLevel: 'debug',
-    provider: 'ExperiencesSDKProvider',
+    provider: PACT_PROVIDER,
     providerVersion: '1.3.0',
   });
 
@@ -286,3 +348,17 @@ describe('Pact Verification', () => {
     });
   });
 });
+
+const renderEditorRoot = () => {
+  addComponentRegistration(componentRegistrations[0]);
+  addComponentRegistration(componentRegistrations[1]);
+
+  const { result } = renderHook(() =>
+    useTreeStore((state) => ({
+      updateTree: state.updateTree,
+    })),
+  );
+  act(() => result.current.updateTree(tree));
+
+  render(<VisualEditorRoot experience={experience} />);
+};
