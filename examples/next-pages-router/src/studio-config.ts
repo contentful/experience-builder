98bf334b4fedf35c074749b4f087bd11c859a766
Ely Lucas <ely@meta-tek.net>
2024-07-12 09:25:37 -0600

    chore: update nextjs apps to use defineComponents (#693)
    * chore: update nextjs apps to use defineComponents
    
    * chore: rename eb-config files to studio-config

---
+++ examples/next-pages-router/src/studio-config.ts
@@ -0,0 +1,21 @@
+import { defineComponents } from '@contentful/experiences-sdk-react';
+
+defineComponents([
+  // Add your custom components here
+  // example:
+  // {
+  //   component: Button,
+  //   definition: {
+  //     id: 'custom-button',
+  //     name: 'Button',
+  //     category: 'Custom Components',
+  //     variables: {
+  //       text: {
+  //         displayName: 'Text',
+  //         type: 'Text',
+  //         defaultValue: 'Click me'
+  //       },
+  //     },
+  //   },
+  // },
+]);
