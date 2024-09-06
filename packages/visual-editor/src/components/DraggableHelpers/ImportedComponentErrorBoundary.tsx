64818850a9fc8d0f26601f564e5ada253a732e4e
Thomas Kellermeier <Chaoste@users.noreply.github.com>
2024-09-06 15:08:07 +0200

    Revert "fix: dont swallow errors that are originated inside the SDK [SPA-2189]"

--- packages/visual-editor/src/components/DraggableHelpers/ImportedComponentErrorBoundary.tsx
+++ packages/visual-editor/src/components/DraggableHelpers/ImportedComponentErrorBoundary.tsx
@@ -7,23 +7,8 @@ class ImportedComponentError extends Error {
   }
 }
 
-/** Use this error class (inside visual-editor) if you want to make sure that the error
- * is tracked via Sentry. Currently, the `ImportedComponentErrorBoundary` is swallowing
- * more errors than intended, so this way we make sure that the errors are being tracked. */
-export class SDKVisualEditorError extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = 'SDKVisualEditorError';
-  }
-}
-
 export class ImportedComponentErrorBoundary extends React.Component<{ children: ReactElement }> {
   componentDidCatch(error: Error, _errorInfo: ErrorInfo) {
-    if (error instanceof SDKVisualEditorError) {
-      // Turning it into ImportedComponentError skips it during error tracking. By explicitly creating
-      // a SDKVisualEditorError, we can make sure that errors in visual-editor are still being tracked.
-      throw error;
-    }
     const err = new ImportedComponentError(error.message);
     err.stack = error.stack;
     throw err;
