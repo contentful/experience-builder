f8dab9f4f24fcf4143de8e660ada860633c363f7
Ely Lucas <ely@meta-tek.net>
2024-09-10 08:45:21 -0600

    chore: add design tokens

--- packages/test-apps/react-vite/src/studio-config.ts
+++ packages/test-apps/react-vite/src/studio-config.ts
@@ -1,4 +1,8 @@
-import { defineComponents, defineBreakpoints } from '@contentful/experiences-sdk-react';
+import {
+  defineComponents,
+  defineBreakpoints,
+  defineDesignTokens,
+} from '@contentful/experiences-sdk-react';
 import ComponentWithChildren from './components/ComponentWithChildren';
 import { LinkComponent } from './components/LinkComponent';
 import { CustomImageComponent } from './components/CustomImageComponent';
@@ -192,3 +196,29 @@ defineBreakpoints([
     previewSize: '350px',
   },
 ]);
+
+defineDesignTokens({
+  spacing: { XS: '4px', S: '16px', M: '32px', L: '64px', XL: '128px' },
+  sizing: { XS: '16px', S: '100px', M: '300px', L: '600px', XL: '1024px' },
+  color: {
+    MyGreen: '#00ff00',
+    Azure: 'azure',
+    Orange: '#cacaca',
+    Blue: '#0000ff',
+    MyCssVar: 'var(--my-css-var)',
+    MyRgba: 'rgba(255, 0, 0, 0.5)',
+    MyRgb: 'rgb(255, 100, 100)',
+    MyBadName: '#RR9833',
+    LightGoldenRodYellow: 'lightgoldenrodyellow',
+  },
+  border: {
+    Azure: { width: '1px', style: 'solid', color: 'azure' },
+    Hero: { width: '2px', style: 'dashed', color: '#ffaabb' },
+    Card: { width: '1px', style: 'solid', color: '#ffccbb' },
+    Carousel: { width: '2px', style: 'dotted', color: 'rgba(30, 25, 25, 0.75)' },
+  },
+  fontSize: { XS: '12px', SM: '14px', MD: '16px', LG: '24px', XL: '32px' },
+  lineHeight: { XS: '1', SM: '1.25', MD: '1.5', LG: '200%' },
+  letterSpacing: { None: '0', XS: '0.05em', SM: '0.1em', MD: '0.15em', LG: '0.2em' },
+  textColor: { Dark: '#1a1a1a', Light: '#efefdd', Slate: '#94a3b8' },
+});
