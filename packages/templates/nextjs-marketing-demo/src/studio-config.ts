e3b1ebcce779f20f99778b0f98742ebee21f23d4
ryunsong-contentful <ryun.song@contentful.com>
2024-09-24 16:11:18 -0600

    feat: add form login custom component

--- packages/templates/nextjs-marketing-demo/src/studio-config.ts
+++ packages/templates/nextjs-marketing-demo/src/studio-config.ts
@@ -1,7 +1,11 @@
 import { defineComponents } from '@contentful/experiences-sdk-react';
 import { ButtonComponentRegistration } from './components/ButtonComponentRegistration';
 import { CustomRatingComponentRegistration } from '@/components/CustomRatingComponentRegistration';
+import { FormLoginComponentRegistration } from '@/components/FormLoginComponentRegistration';
 
-defineComponents([ButtonComponentRegistration, CustomRatingComponentRegistration], {
-  enabledBuiltInComponents: [],
-});
+defineComponents(
+  [ButtonComponentRegistration, CustomRatingComponentRegistration, FormLoginComponentRegistration],
+  {
+    enabledBuiltInComponents: [],
+  },
+);
