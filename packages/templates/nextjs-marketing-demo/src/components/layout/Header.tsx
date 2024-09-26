044a4375a6c5a6351642873f4ef061979b9bd524
Adrian Meyer <8539634+primeinteger@users.noreply.github.com>
2024-09-26 14:47:35 -0600

    feat(nextjs-marketing-demo): add Header, Footer, and light mode styling [ALT-1270] (#764)

---
+++ packages/templates/nextjs-marketing-demo/src/components/layout/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button } from 'antd';
+import Nav from './Nav';
+import styles from './header.module.css';
+import Logo from '@/assets/Logo.svg';
+
+const Header: React.FC = () => {
+  return (
+    <div className={styles.header}>
+      <Logo />
+      <div className={styles.nav}>
+        <Nav />
+        <Button variant="outlined" size="small">
+          Book Now
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
