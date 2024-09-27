1fd2f597059a595bd07c64ddfc790986b59a2f71
Adrian Meyer <8539634+primeinteger@users.noreply.github.com>
2024-09-27 09:09:17 -0600

    feat: add Card custom component
    chore: update readme
    
    fix: routing for Preview URL

--- packages/templates/nextjs-marketing-demo/src/middleware.ts
+++ packages/templates/nextjs-marketing-demo/src/middleware.ts
@@ -1,13 +1,23 @@
-import { i18nRouter } from 'next-i18n-router';
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
   const i18n = {
     locales: ['en-US', 'de'],
     defaultLocale: 'en-US',
   };
-  const resp = i18nRouter(request, i18n);
-  return resp;
+
+  const url = request.nextUrl.clone();
+  const pathname = url.pathname;
+  const segments = pathname.split('/');
+  const locale = segments[1];
+
+  if (i18n.locales.includes(locale)) {
+    const newPathname = `/${segments.slice(2).join('/')}`;
+    url.pathname = newPathname;
+    return NextResponse.rewrite(new URL(`/${locale}${newPathname}`, request.url));
+  }
+
+  return NextResponse.redirect(new URL(`/${i18n.defaultLocale}${pathname}`, request.url));
 }
 
 // only applies this middleware to files in the app directory
