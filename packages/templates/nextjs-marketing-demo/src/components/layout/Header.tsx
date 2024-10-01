ce814e3fc06e7120ea011421774eca258abcf47b
Adrian Meyer <8539634+primeinteger@users.noreply.github.com>
2024-10-01 13:34:23 -0600

    feat: hide nav on mobile in marketing demo app [ALT-1295]

--- packages/templates/nextjs-marketing-demo/src/components/layout/Header.tsx
+++ packages/templates/nextjs-marketing-demo/src/components/layout/Header.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Button } from 'antd';
 import Nav from './Nav';
 import styles from './header.module.css';
 import Logo from '@/assets/Logo.svg';
@@ -8,12 +7,7 @@ const Header: React.FC = () => {
   return (
     <div className={styles.header}>
       <Logo />
-      <div className={styles.nav}>
-        <Nav />
-        <Button variant="outlined" size="small">
-          Book Now
-        </Button>
-      </div>
+      <Nav />
     </div>
   );
 };
