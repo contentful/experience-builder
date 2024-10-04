4a57fe643b1732d1be4d077acb19fef2f0d95c43
Adrian Meyer <8539634+primeinteger@users.noreply.github.com>
2024-10-04 15:13:46 -0600

    fix: move margin to wrapperStyles when drag wrapper is enabled [ALT-1293]

--- packages/components/src/components/Divider/ContentfulDivider.tsx
+++ packages/components/src/components/Divider/ContentfulDivider.tsx
@@ -1,22 +1,14 @@
 import React from 'react';
 import './ContentfulDivider.css';
-import { css, cx } from 'emotion';
-
-const styles = {
-  hr: css({
-    border: 'none',
-  }),
-};
 
 export type ContentfulDividerProps = {
   className?: string;
 };
 
-export const ContentfulDivider = (props: ContentfulDividerProps) => {
-  const { className } = props;
+export const ContentfulDivider = ({ className = '', ...props }: ContentfulDividerProps) => {
   return (
     <div className="cf-divider" {...props}>
-      <hr className={cx(styles.hr, className)} />
+      <hr className={className} />
     </div>
   );
 };
