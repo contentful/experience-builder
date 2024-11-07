a45d6441cc3bea57836ded6aa4f08346ec4c1c39
George Chios <george.chios@contentful.com>
2024-11-07 09:47:29 -0700

    chore: adds version history as a feature

--- packages/experience-builder-sdk/src/core/sdkFeatures.ts
+++ packages/experience-builder-sdk/src/core/sdkFeatures.ts
@@ -1,5 +1,6 @@
 // Object to store the SDK features that are enabled/disabled
 export const sdkFeatures: Record<string, unknown> = {
   hasSDKVersionUI: true,
+  hasVersionHistory: true,
   cfVisibility: true,
 };
