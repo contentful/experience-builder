91268151e2bba78e870e0fd9a5e55484801de1b1
Adrian Meyer <8539634+primeinteger@users.noreply.github.com>
2024-11-22 19:41:44 -0700

    chore: fix import warning for react/package.json

--- packages/templates/nextjs-marketing-demo/src/components/Footer/FooterDebugging.tsx
+++ packages/templates/nextjs-marketing-demo/src/components/Footer/FooterDebugging.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import React from 'react';
+import reactPackage from 'react/package.json';
 import { useSearchParams } from 'next/navigation';
 import { version as sdkVersion } from '@contentful/experiences-sdk-react';
-import { version as reactVersion } from 'react/package.json';
 import { Divider, Flex, Typography } from 'antd';
 import styles from './styles.module.css';
 
@@ -19,7 +19,7 @@ const FooterDebugging: React.FC = () => {
     <Flex align="center" vertical={false} className={styles.debugInfo}>
       <Typography className={styles.darkText}>Experiences SDK v{sdkVersion}</Typography>
       <Divider type="vertical" className={styles.divider} />
-      <Typography className={styles.darkText}>React v{reactVersion}</Typography>
+      <Typography className={styles.darkText}>React v{reactPackage.version}</Typography>
     </Flex>
   );
 };
