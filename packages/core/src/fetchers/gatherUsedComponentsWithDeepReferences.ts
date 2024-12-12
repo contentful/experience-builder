61a8e10da52944e22a96c1a2dc1b4b3358b38330
Anwar <anwaar.lance@gmail.com>
2024-12-12 13:15:41 +0100

    fix: typo

--- packages/core/src/fetchers/gatherUsedComponentsWithDeepReferences.ts
+++ packages/core/src/fetchers/gatherUsedComponentsWithDeepReferences.ts
@@ -2,16 +2,16 @@ import { ExperienceEntry, ExperienceFields } from '@/types';
 export const gatherUsedComponentsWithDeepRefernces = (
   experienceEntryFields?: ExperienceFields,
 ): ExperienceEntry[] => {
-  const usedComponentdeepReferences: ExperienceEntry[] = [];
+  const usedComponentDeepReferences: ExperienceEntry[] = [];
   const usedComponents = experienceEntryFields?.usedComponents as ExperienceEntry[];
   if (!usedComponents || usedComponents.length === 0) {
     return [];
   }
   for (const component of usedComponents) {
     if ('fields' in component) {
-      usedComponentdeepReferences.push(component);
-      usedComponentdeepReferences.push(...gatherUsedComponentsWithDeepRefernces(component.fields));
+      usedComponentDeepReferences.push(component);
+      usedComponentDeepReferences.push(...gatherUsedComponentsWithDeepRefernces(component.fields));
     }
   }
-  return usedComponentdeepReferences;
+  return usedComponentDeepReferences;
 };
