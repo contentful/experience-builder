77078c8791824648d968cc667bf1a2e5c3da587b
Thomas Kellermeier <thomas.kellermeier@contentful.com>
2024-12-19 09:09:15 +0100

    fix: handle undefined cfSsrClassName

--- packages/visual-editor/src/hooks/useBreakpoints.ts
+++ packages/visual-editor/src/hooks/useBreakpoints.ts
@@ -1,9 +1,4 @@
-import type {
-  ValuesByBreakpoint,
-  Breakpoint,
-  PrimitiveValue,
-  ResolveDesignValueType,
-} from '@contentful/experiences-core/types';
+import type { Breakpoint, ResolveDesignValueType } from '@contentful/experiences-core/types';
 import {
   mediaQueryMatcher,
   getFallbackBreakpointIndex,
