397e714a222ec143fda55297aaac9b0ad45442a1
Chase Poirier <chase.poirier@contentful.com>
2025-01-07 09:57:45 -0700

    chore: remove type field from heading built-in

--- packages/components/src/components/Heading/index.ts
+++ packages/components/src/components/Heading/index.ts
@@ -66,22 +66,5 @@ export const HeadingComponentDefinition: ComponentDefinition = {
       description: 'The text to display in the heading.',
       defaultValue: 'Heading',
     },
-    type: {
-      displayName: 'Type',
-      type: 'Text',
-      defaultValue: 'h1',
-      description:
-        'Determines the HTML tag of the heading. Value can be h1, h2, h3, h4, h5, or h6.',
-      validations: {
-        in: [
-          { value: 'h1', displayName: 'H1' },
-          { value: 'h2', displayName: 'H2' },
-          { value: 'h3', displayName: 'H3' },
-          { value: 'h4', displayName: 'H4' },
-          { value: 'h5', displayName: 'H5' },
-          { value: 'h6', displayName: 'H6' },
-        ],
-      },
-    },
   },
 };
