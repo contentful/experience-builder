7d38f554ec54256d736dab1de0a30984317caae0
Chase Poirier <chase.poirier@contentful.com>
2025-01-31 13:39:07 -0700

    chore: update validators for prebinding fields

--- packages/validators/src/schemas/index.ts
+++ packages/validators/src/schemas/index.ts
@@ -1,5 +1,8 @@
 export {
   BindingSourceTypeEnum,
+  PatternProperty,
+  PatternPropertyDefinition,
+  VariableMapping,
   ExperienceFieldsCMAShapeSchema as Schema_2023_09_28,
 } from './v2023_09_28/experience';
 export { ComponentDefinitionSchema } from './componentDefinition';
