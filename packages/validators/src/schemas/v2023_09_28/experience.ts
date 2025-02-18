4ad1235fdd249d0b878c5fdd4331f785cb203737
Chase Poirier <chase.poirier@contentful.com>
2025-02-18 13:15:12 -0700

    Revert "chore: add .strict to novalue type"
    This reverts commit 6822184ae24d6d834eeeb3a3cf4d7418058816e5.

--- packages/validators/src/schemas/v2023_09_28/experience.ts
+++ packages/validators/src/schemas/v2023_09_28/experience.ts
@@ -88,7 +88,7 @@ const ComponentValueSchema = z
 
 // TODO: finalize schema structure before release
 // https://contentful.atlassian.net/browse/LUMOS-523
-const NoValueSchema = z.object({ type: z.literal('NoValue') }).strict();
+const NoValueSchema = z.object({ type: z.literal('NoValue') });
 
 const ComponentPropertyValueSchema = z.discriminatedUnion('type', [
   DesignValueSchema,
