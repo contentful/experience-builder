64d1527615cb4861a28d7e36c93fc8d249108739
Thomas Kellermeier <thomas.kellermeier@contentful.com>
2025-04-22 10:57:11 +0200

    docs: improve documentation and comments on sys.locale

--- packages/core/src/utils/localizeEntity.ts
+++ packages/core/src/utils/localizeEntity.ts
@@ -4,9 +4,13 @@ import { Asset, Entry } from 'contentful';
  * Localizes the provided entry or asset to match the regular format of CDA/CPA entities.
  * Note that this function does not apply a fallback to the default locale nor does it check
  * the content type for the localization setting of each field.
- * It will simply resolve each field to the requested locale.
+ * It will simply resolve each field to the requested locale. As using single and multiple
+ * reference fields is still considered an experimental feature, this function does not handle
+ * recursive localization of deeper referenced entities.
  *
  * If the entity is already localized, it will return the entity as is.
+ *
+ * Note that localization is later on determined by the existence of the `sys.locale` property (matching the API shape).
  *
  * @example
  * ```
@@ -23,7 +27,8 @@ export function localizeEntity<T extends Asset | Entry>(entity: T, locale: strin
     return entity;
   }
   const cloned = structuredClone(entity);
-  // Set the requested locale as entry locale
+  // Set the requested locale as entry locale to follow the API shape:
+  // https://www.contentful.com/developers/docs/references/content-delivery-api/#/introduction/common-resource-attributes
   cloned.sys.locale = locale;
 
   for (const key in cloned.fields) {
