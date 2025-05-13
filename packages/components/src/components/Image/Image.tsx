61f8c85967a2bdcc983ff1f1175ec5ee4c4857fc
Thomas Kellermeier <Chaoste@users.noreply.github.com>
2025-05-13 14:47:26 +0200

    fix: add relative position wrapper for misaligned image placeholder (#1119)

--- packages/components/src/components/Image/Image.tsx
+++ packages/components/src/components/Image/Image.tsx
@@ -10,9 +10,9 @@ export interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
 export const Image: React.FC<ImageProps> = ({ className = '', src, cfImageAsset, ...props }) => {
   if (!cfImageAsset && !src) {
     return (
-      <>
+      <div className={combineClasses('cf-placeholder-wrapper', className)}>
         <img
-          className={combineClasses('cf-placeholder-image', className)}
+          className={'cf-placeholder-image'}
           src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAA"
           {...props}
         />
@@ -26,7 +26,7 @@ export const Image: React.FC<ImageProps> = ({ className = '', src, cfImageAsset,
             d="M13.5 2h-10a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1m-10 1h10v4.836l-1.543-1.543a1 1 0 0 0-1.414 0L3.836 13H3.5zm10 10H5.25l6-6 2.25 2.25zm-7-5.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3m0-2a.5.5 0 1 1 0 1 .5.5 0 0 1 0-1"
           />
         </svg>
-      </>
+      </div>
     );
   }
 
