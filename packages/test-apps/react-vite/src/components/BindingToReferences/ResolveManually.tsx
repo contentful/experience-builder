f6d97ee99a4d4a5bb5087c1ad904c41af606fcd5
Dimitry Kireyenkoff <dimitry.kireyenkoff@contentful.com>
2025-05-26 14:24:35 +0100

    fix: makes component ResolveManually.tsx self contained

--- packages/test-apps/react-vite/src/components/BindingToReferences/ResolveManually.tsx
+++ packages/test-apps/react-vite/src/components/BindingToReferences/ResolveManually.tsx
@@ -1,17 +1,17 @@
+/* eslint-disable react-refresh/only-export-components */
 import React from 'react';
-import type { Asset, Entry } from 'contentful';
-import { type ComponentDefinition } from '@contentful/experiences-sdk-react';
+import type { Asset, Entry, UnresolvedLink } from 'contentful';
+import { type ComponentDefinition, inMemoryEntities } from '@contentful/experiences-sdk-react';
+import { isLink, isAsset, isEntry, isArrayOfLinks } from '@contentful/experiences-core';
 import { stringifyCompact } from '../../utils/debugging';
-import { resolveEntityReferences } from '../../binding-to-reference-fields/resolutionUtils';
 
 type Item = Entry | Asset;
 
 type ResolveManuallyProps = {
-  items?: (Item | null)[]; // TODO: in theory this could be arrays of nulls (as currently items not in entity store eg. archived will not be resolved).
-  singleItem?: Item | null; // TODO: could this be null? eg. when binding link points to a archived entry?
+  items?: Item[];
+  singleItem?: Item;
 };
 
-// eslint-disable-next-line react-refresh/only-export-components
 export const ResolveManuallyComponentDefinition: ComponentDefinition = {
   id: 'bind-example-resolve-manually',
   name: 'Resolve Manually',
@@ -34,11 +34,15 @@ export const ResolveManuallyComponent: React.FC<ResolveManuallyProps> = (
 ) => {
   const { items: shallowItems, singleItem: shallowSingleItem } = props;
 
-  // could be undefined...
+  // could be undefined when:
+  //  - user didn't bind
+  //  - bound to empty reference
+  //  - bound to reference to archived entity
+  //  - bound to array which only contains references to archived entities
   const items = (shallowItems || []).map((shallowItem) =>
-    resolveEntityReferences(shallowItem || undefined),
-  ); // forcing null to undefined
-  const singleItem = shallowSingleItem ? resolveEntityReferences(shallowSingleItem) : undefined;
+    resolveEntityLinks(shallowItem || undefined),
+  );
+  const singleItem = shallowSingleItem ? resolveEntityLinks(shallowSingleItem) : undefined;
   return (
     <div>
       <h2>Collection of items</h2>
@@ -54,3 +58,42 @@ export const ResolveManuallyComponent: React.FC<ResolveManuallyProps> = (
 };
 
 export default ResolveManuallyComponent;
+
+export const resolveEntityLinks = (
+  entity: Entry | Asset | undefined,
+): Entry | Asset | undefined => {
+  if (entity === undefined) {
+    return undefined;
+  }
+  if (isAsset(entity)) {
+    return structuredClone(entity);
+  }
+  if (isEntry(entity)) {
+    const e = structuredClone(entity) as Entry;
+    for (const [fname, value] of Object.entries(e.fields)) {
+      // @ts-expect-error casting unkonwn
+      e.fields[fname] = resolveLinkOrArrayOrPassthrough(value); // default behaviour when link is not resolved is to return undefined
+    }
+    return e;
+  }
+
+  // everything else just pass through, but copy
+  return structuredClone(entity);
+};
+
+export const resolveLinkOrArrayOrPassthrough = (
+  fieldValue: unknown | UnresolvedLink<'Asset'> | UnresolvedLink<'Entry'>,
+) => {
+  if (isLink(fieldValue)) {
+    return inMemoryEntities.maybeResolveLink(
+      fieldValue as unknown as UnresolvedLink<'Asset'> | UnresolvedLink<'Entry'>,
+    );
+  }
+
+  if (isArrayOfLinks(fieldValue)) {
+    return fieldValue.map((link) => inMemoryEntities.maybeResolveLink(link));
+  }
+
+  // we just pass through the value
+  return fieldValue;
+};
