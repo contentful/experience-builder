6a832e005caa287226213de61cef956b94c5527f
Chinenye Onuegbu <chinenye.onuegbu@contentful.com>
2025-05-27 18:19:59 +0200

    fix: export VariableMapping from the correct location

--- packages/validators/src/schemas/index.ts
+++ packages/validators/src/schemas/index.ts
@@ -1,10 +1,10 @@
 export {
   BindingSourceTypeEnum,
-  VariableMapping,
   ExperienceFieldsCMAShapeSchema as ExperienceSchema_2023_09_28,
 } from './v2023_09_28/experience';
 export {
   PatternFieldsCMAShapeSchema as PatternSchema_2023_09_28,
   type PatternPropertyDefinition,
+  VariableMapping,
 } from './v2023_09_28/pattern';
 export { ComponentDefinitionSchema } from './componentDefinition';
