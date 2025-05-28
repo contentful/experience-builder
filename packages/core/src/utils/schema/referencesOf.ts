737ee2f0c5ceca66c4b3d87aa6624b0d741d498f
Dimitry Kireyenkoff <dimitry.kireyenkoff@contentful.com>
2025-05-28 09:20:05 +0100

    fix: adds referencesOf and exports api

---
+++ packages/core/src/utils/schema/referencesOf.ts
@@ -0,0 +1,66 @@
+import type { Entry, UnresolvedLink } from 'contentful';
+
+type Link<T extends 'Asset' | 'Entry' = 'Asset' | 'Entry'> = UnresolvedLink<T>;
+
+export type FnShouldFollowReferencesOfEntryField = (fieldName: string, entry: Entry) => boolean;
+
+const uniqueById = <T extends { sys: { id: string } }>(arr: Array<T>): T[] => {
+  const map = new Map<string, T>();
+  arr.forEach((item) => map.set(item.sys.id, item));
+  return [...map.values()];
+};
+
+const isObject = (value: unknown) => {
+  return typeof value === 'object' && value !== null;
+};
+
+/**
+ * Extracts all references from an entry.
+ * Handles both: reference and multi-reference fields.
+ * Returns unique array of references (even if they repeat within the entry).
+ */
+export const referencesOf = (
+  entry: Entry,
+  fnShouldFollowReferencesOfEntryField?: FnShouldFollowReferencesOfEntryField,
+): UnresolvedLink<'Asset' | 'Entry'>[] => {
+  const references: Link<'Asset' | 'Entry'>[] = [];
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const handleArray = (fieldValue: Array<unknown>, _fieldName: string) => {
+    for (const item of fieldValue) {
+      if (isObject(item) && (item as Link).sys?.type === 'Link') {
+        references.push(item as Link<'Asset' | 'Entry'>);
+      }
+    }
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const handleLink = (fieldValue: Link<'Asset' | 'Entry'>, _fieldName: string) => {
+    references.push(fieldValue);
+  };
+
+  for (const [fieldName, fieldValue] of Object.entries(
+    entry.fields as Record<string, Record<string, unknown>>,
+  )) {
+    if (
+      fnShouldFollowReferencesOfEntryField &&
+      !fnShouldFollowReferencesOfEntryField(fieldName, entry)
+    ) {
+      continue;
+    }
+    if (fieldValue === undefined) {
+      continue; // edge case when field is present on object, but is set to undefined explicitly e.g. during test mocking { myField: undefined }
+    }
+
+    if (Array.isArray(fieldValue)) {
+      handleArray(fieldValue, fieldName);
+    } else if (
+      fieldValue !== null &&
+      isObject(fieldValue) &&
+      (fieldValue as Link).sys?.type === 'Link'
+    ) {
+      handleLink(fieldValue as Link<'Asset' | 'Entry'>, fieldName);
+    }
+  }
+  return uniqueById(references);
+};
