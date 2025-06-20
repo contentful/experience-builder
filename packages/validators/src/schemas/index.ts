2128bcfa8ac2c0394bb69a37c0e4950bb2905b2f
Chinenye Onuegbu <chinenye.onuegbu@contentful.com>
2025-06-20 15:32:33 +0200

    refactor: rename PatternPropertyDefinition to ParameterDefinition

--- packages/validators/src/schemas/index.ts
+++ packages/validators/src/schemas/index.ts
@@ -4,7 +4,7 @@ export {
 } from './v2023_09_28/experience';
 export {
   PatternFieldsCMAShapeSchema as PatternSchema_2023_09_28,
-  type PatternPropertyDefinition,
+  type ParameterDefinition,
   VariableMapping,
 } from './v2023_09_28/pattern';
 export { ComponentDefinitionSchema } from './componentDefinition';
