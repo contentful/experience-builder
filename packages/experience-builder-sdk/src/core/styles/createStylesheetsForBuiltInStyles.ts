1cc148f44240061dadc2aa88671b2a93dfe7847d
Thomas Kellermeier <Chaoste@users.noreply.github.com>
2025-07-02 09:57:58 +0200

    fix: remove old PreviewUnboundImage and unnnecessary special casing (#1171)

--- packages/experience-builder-sdk/src/core/styles/createStylesheetsForBuiltInStyles.ts
+++ packages/experience-builder-sdk/src/core/styles/createStylesheetsForBuiltInStyles.ts
@@ -4,15 +4,13 @@ import {
   flattenDesignTokenRegistry,
   maybePopulateDesignTokenValue,
   stringifyCssProperties,
-  toMediaQuery,
   buildCfStyles,
   transformVisibility,
 } from '@contentful/experiences-core';
 import { Breakpoint, ComponentTreeNode, PrimitiveValue } from '@contentful/experiences-core/types';
 import md5 from 'md5';
-import { useMemo } from 'react';
 
-type ResolvedStylesheetData = Array<{
+export type ResolvedStylesheetData = Array<{
   className: string;
   breakpointCondition: string;
   css: string;
@@ -128,78 +126,4 @@ export const createStylesheetsForBuiltInStyles = ({
   }
 
   return result;
-};
-
-/**
- * Takes the CSS code for each breakpoint and merges them into a single CSS string.
- * It will wrap each breakpoint's CSS code in a media query (exception: default breakpoint with '*').
- *
- * **Example Input:**
- * ```
- * [
- *  { className: 'cfstyles-123', breakpointCondition: '*', css: 'color:red;', visibilityCss: 'display:none !important;' },
- *  { className: 'cfstyles-456', breakpointCondition: '<768px', css: 'color:blue;' },
- * ]
- * ```
- *
- * **Example Output:**
- * ```
- * '.cfstyles-123{color:red;}@media not (max-width:768px){.cfstyles-123{display:none !important;}}@media(max-width:768px){.cfstyles-456{color:blue;}}'
- * ```
- */
-export const convertResolvedDesignValuesToMediaQuery = (stylesheetData: ResolvedStylesheetData) => {
-  const stylesheet = stylesheetData.reduce(
-    (acc, { breakpointCondition, className, css, visibilityCss }, index) => {
-      if (acc.classNames.includes(className)) {
-        return acc;
-      }
-
-      const mediaQueryCss = toMediaQuery({
-        condition: breakpointCondition,
-        cssByClassName: { [className]: css },
-      });
-      const visibilityMediaQueryCss = toMediaQuery({
-        condition: breakpointCondition,
-        cssByClassName: { [className]: visibilityCss ?? '' },
-        // Validation ensures that it starts with the '*' breakpoint
-        nextCondition: stylesheetData[index + 1]?.breakpointCondition,
-      });
-      return {
-        classNames: [...acc.classNames, className],
-        css: `${acc.css}${mediaQueryCss}${visibilityMediaQueryCss}`,
-      };
-    },
-    {
-      classNames: [] as string[],
-      css: '',
-    },
-  );
-
-  return {
-    css: stylesheet.css,
-    className: stylesheet.classNames.join(' '),
-  };
-};
-
-export const useMediaQuery = ({
-  designPropertiesByBreakpoint,
-  breakpoints,
-  node,
-  patternRootNodeIdsChain,
-}: {
-  designPropertiesByBreakpoint: Record<string, Record<string, any>>;
-  breakpoints: Breakpoint[];
-  node: ComponentTreeNode;
-  patternRootNodeIdsChain?: string;
-}) => {
-  return useMemo(() => {
-    const stylesheetData = createStylesheetsForBuiltInStyles({
-      designPropertiesByBreakpoint,
-      breakpoints,
-      node,
-      patternRootNodeIdsChain,
-    });
-
-    return convertResolvedDesignValuesToMediaQuery(stylesheetData);
-  }, [designPropertiesByBreakpoint, breakpoints, node, patternRootNodeIdsChain]);
 };
