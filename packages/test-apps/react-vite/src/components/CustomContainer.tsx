a0c0faba399ef9f294a91bb6bbcd5efe0264dd82
Daniel Vasylenko <connect.danv@gmail.com>
2026-08-28 15:55:13 +0200

    fix: migrate from deprecated emotion package

--- packages/test-apps/react-vite/src/components/CustomContainer.tsx
+++ packages/test-apps/react-vite/src/components/CustomContainer.tsx
@@ -1,14 +1,17 @@
 import { containerDefinition, ContentfulContainer } from '@contentful/experiences-components-react';
 import { ComponentDefinition } from '@contentful/experiences-sdk-react';
 import React from 'react';
-import { css, cx } from 'emotion';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const CustomContainer: React.FC<any> = (props) => {
   const { className, minHeight, ...otherProps } = props;
-  const customClassName = css({ minHeight: `${minHeight} !important` });
-  const combinedClassName = cx(className, customClassName);
-  return <ContentfulContainer className={combinedClassName} {...otherProps} />;
+  return (
+    <ContentfulContainer
+      className={className}
+      style={minHeight ? { minHeight } : undefined}
+      {...otherProps}
+    />
+  );
 };
 
 export const customContainerDefinition = {
